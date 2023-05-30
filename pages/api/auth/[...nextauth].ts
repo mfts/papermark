76aa72abb0ae1eb0a295192d5b5c76edcd004b91
Marc Seitz <marc@mfts.io>
2023-05-30 13:15:07 +0300

    fix: change domains

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -23,7 +23,7 @@ export const authOptions: NextAuthOptions = {
         sameSite: "lax",
         path: "/",
         // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
-        domain: VERCEL_DEPLOYMENT ? "everything.to" : undefined,
+        domain: VERCEL_DEPLOYMENT ? "www.papermark.io" : undefined,
         secure: VERCEL_DEPLOYMENT,
       },
     },
