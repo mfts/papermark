d65da2da0d2080fbdd5bb1f6f79634cad9022750
Marc Seitz <marc@mfts.io>
2023-05-30 16:32:13 +0300

    fix emailform background color

--- components/EmailForm.tsx
+++ components/EmailForm.tsx
@@ -1,7 +1,7 @@
 export default function EmailForm({ onSubmitHandler, setEmail }: any) {
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-black">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-10 text-2xl font-bold leading-9 tracking-tight text-white">
             Enter your email to view the document
