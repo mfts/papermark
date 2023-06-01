415a7686922f05f27b402baa8344af8d6164382b
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-06-01 05:54:19 +0000

    Add component when there are no documents available

--- pages/documents.tsx
+++ pages/documents.tsx
@@ -2,6 +2,7 @@ import Sidebar from "@/components/Sidebar";
 import useDocuments from "@/lib/swr/use-documents";
 import DocumentCard from "@/components/documents/document-card";
 import Skeleton from "@/components/Skeleton";
+import { PlusIcon } from "@heroicons/react/24/solid";
 
 export default function Documents() {
   const { documents } = useDocuments();
@@ -10,12 +11,18 @@ export default function Documents() {
     <>
       <div>
         <Sidebar>
-          <main className="">
+          <main className="h-fit">
             <header className="flex items-center justify-between border-b border-white/5 px-4 py-4 sm:px-6 sm:py-6 lg:px-8">
               <h1 className="text-base font-semibold leading-7 text-white">
                 My Documents
               </h1>
             </header>
+
+            {documents && documents.length === 0 && (
+              <div className="flex items-center justify-center h-96">
+                <EmptyDocuments />
+              </div>
+            )}
 
             {/* Documents list */}
             <ul role="list" className="divide-y divide-white/5">
@@ -39,5 +46,40 @@ export default function Documents() {
         </Sidebar>
       </div>
     </>
+  );
+}
+
+export function EmptyDocuments() {
+  return (
+    <div className="text-center">
+      <svg
+        className="mx-auto h-12 w-12 text-gray-400"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          vectorEffect="non-scaling-stroke"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+        />
+      </svg>
+      <h3 className="mt-2 text-sm font-medium text-white">No documents</h3>
+      <p className="mt-1 text-sm text-gray-400">
+        Get started by uploading a new document.
+      </p>
+      <div className="mt-6">
+        <button
+          type="button"
+          className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+          New Document
+        </button>
+      </div>
+    </div>
   );
 }
