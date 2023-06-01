3ba2a8ab484bd84f22fd670f6980132b62f3b192
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-06-01 05:56:47 +0000

    Remove new link from navbar

--- pages/documents.tsx
+++ pages/documents.tsx
@@ -72,13 +72,14 @@ export function EmptyDocuments() {
         Get started by uploading a new document.
       </p>
       <div className="mt-6">
-        <button
+        <Link
+          href="/documents/new"
           type="button"
           className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
           New Document
-        </button>
+        </Link>
       </div>
     </div>
   );
