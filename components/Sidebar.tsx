6e25caba3616251aab4c291888f557dc9c684e24
Marc Seitz <marc@mfts.io>
2023-06-06 16:53:14 +0200

    fix: lint error

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -92,22 +92,24 @@ export default function Sidebar({ children }: { children: React.ReactNode }) {
                       <li>
                         <ul role="list" className="-mx-2 space-y-1">
                           {navigation.map((item) => (
-                            <button
-                              onClick={() => router.push(item.href)}
-                              className={classNames(
-                                item.current
-                                  ? "bg-gray-800 text-white"
-                                  : "text-gray-400 hover:text-white hover:bg-gray-800",
-                                "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold w-full"
-                              )}
-                              disabled={item.disabled}
-                            >
-                              <item.icon
-                                className="h-6 w-6 shrink-0"
-                                aria-hidden="true"
-                              />
-                              {item.name}
-                            </button>
+                            <li key={item.name}>
+                              <button
+                                onClick={() => router.push(item.href)}
+                                className={classNames(
+                                  item.current
+                                    ? "bg-gray-800 text-white"
+                                    : "text-gray-400 hover:text-white hover:bg-gray-800",
+                                  "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold w-full"
+                                )}
+                                disabled={item.disabled}
+                              >
+                                <item.icon
+                                  className="h-6 w-6 shrink-0"
+                                  aria-hidden="true"
+                                />
+                                {item.name}
+                              </button>
+                            </li>
                           ))}
                         </ul>
                       </li>
