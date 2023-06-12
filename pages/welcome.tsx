96f2d8553125973d83fbd1ea46fc7149413705ab
Marc Seitz <marc@mfts.io>
2023-06-12 12:01:36 +0200

    add AnimatePresence

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -3,12 +3,14 @@ import Intro from "@/components/welcome/intro";
 import Next from "@/components/welcome/next";
 import Upload from "@/components/welcome/upload";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { AnimatePresence } from "framer-motion";
 
 export default function Welcome() {
   const router = useRouter();
 
+
   return (
-    <div className="flex h-screen flex-col items-center justify-center max-w-3xl mx-auto overflow-x-hidden">      
+    <div className="flex h-screen flex-col items-center justify-center max-w-3xl mx-auto overflow-x-hidden">
       <div
         className="absolute inset-x-0 top-10 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
         aria-hidden="true"
@@ -21,10 +23,10 @@ export default function Welcome() {
           }}
         />
       </div>
-      <div>
+      <AnimatePresence mode="wait">
         {router.query.type ? (
           <button
-            className="group absolute left-2 top-10 z-40 rounded-full p-2 transition-all hover:bg-gray-400"
+            className="group absolute left-2 sm:left-10 top-10 z-40 rounded-full p-2 transition-all hover:bg-gray-400"
             onClick={() => router.back()}
           >
             <ArrowLeftIcon className="h-8 w-8 text-gray-500 group-hover:text-gray-800 group-active:scale-90" />
@@ -35,7 +37,7 @@ export default function Welcome() {
         {router.query.type === "next" && <Next key="next" />}
         {router.query.type === "pitchdeck" && <Upload key="pitchdeck" />}
         {router.query.type === "document" && <Upload key="document" />}
-      </div>
+      </AnimatePresence>
     </div>
   );
 }
