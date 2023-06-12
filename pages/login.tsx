f67169b8839df076d8148967df47d4de5a06d764
Marc Seitz <marc@mfts.io>
2023-06-13 00:59:17 +0200

    improve signin / signup page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,5 @@
 import { signIn } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Login() {
@@ -19,12 +20,16 @@ export default function Login() {
           }}
         />
       </div>
-      <div className="z-10 mt-[calc(30vh)] h-fit w-full max-w-md overflow-hidden border border-gray-900 rounded-lg sm:shadow-xl">
+      <div className="z-10 mt-[calc(30vh)] h-fit w-full mx-5 sm:mx-0 max-w-md overflow-hidden border border-gray-900 rounded-lg sm:shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 bg-gray-800 px-4 py-6 pt-8 text-center sm:px-16">
-          <h3 className="text-xl text-white font-semibold">Sign in to Papermark</h3>
-          <p className="text-sm text-gray-400">
-            Start sharing documents with insights.
-          </p>
+          <Link href="/">
+            <span className="text-xl font-bold tracking-tighter text-white">
+              Papermark
+            </span>
+          </Link>
+          <h3 className="text-2xl text-white font-semibold">
+            Start sharing documents
+          </h3>
         </div>
         <div className="flex flex-col bg-gray-800 px-4 py-8 sm:px-16">
           <button
@@ -33,9 +38,17 @@ export default function Login() {
                 ...(next && next.length > 0 ? { callbackUrl: next } : {}),
               });
             }}
-            className="rounded px-10 py-2 font-medium transition-colors text-gray-900 bg-gray-100 hover:text-gray-100 hover:bg-gray-500"
+            className="rounded px-10 py-2 font-medium flex justify-center items-center transition-colors border border-white text-gray-900 bg-white hover:text-gray-100 hover:bg-gray-500"
           >
-            Continue with Google
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 488 512"
+              fill="currentColor"
+              className="h-4 w-4 mr-2"
+            >
+              <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
+            </svg>
+            <span>Continue with Google</span>
           </button>
         </div>
       </div>
