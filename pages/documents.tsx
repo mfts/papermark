7fda4c4e2002b4f122d36227dfc1e058c3d8e6e3
Marc Seitz <marc@mfts.io>
2023-06-13 23:02:32 +0200

    add new documents header with new document button

--- pages/documents.tsx
+++ pages/documents.tsx
@@ -3,22 +3,40 @@ import useDocuments from "@/lib/swr/use-documents";
 import DocumentCard from "@/components/documents/document-card";
 import Skeleton from "@/components/Skeleton";
 import { PlusIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
 export default function Documents() {
   const { documents } = useDocuments();
 
-  const emptyDocument = documents.length === 0;
-
   return (
     <>
       <div>
-        <Sidebar emptyDocument={emptyDocument}>
+        <Sidebar>
           <main className="h-fit">
-            <header className="flex items-center justify-between border-b border-white/5 px-4 py-4 sm:px-6 sm:py-6 lg:px-8">
-              <h1 className="text-base font-semibold leading-7 text-white">
-                My Documents
-              </h1>
-            </header>
+            <div className="border-b border-white/5 px-4 py-5 sm:px-6 lg:px-8">
+              <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap">
+                <div className="ml-4 mt-2">
+                  <h3 className="text-base font-semibold leading-6 text-white">
+                    My Documents
+                  </h3>
+                </div>
+                {documents && documents.length !== 0 && (
+                  <div className="ml-4 mt-2 flex-shrink-0">
+                    <Link
+                      href="/documents/new"
+                      type="button"
+                      className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      <PlusIcon
+                        className="-ml-1 mr-2 h-5 w-5"
+                        aria-hidden="true"
+                      />
+                      New Document
+                    </Link>
+                  </div>
+                )}
+              </div>
+            </div>
 
             {documents && documents.length === 0 && (
               <div className="flex items-center justify-center h-96">
