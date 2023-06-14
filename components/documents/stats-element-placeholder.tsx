f03fa3a5087a4c812671a08cafc39a484f58ccc2
Marc Seitz <marc@mfts.io>
2023-06-14 12:59:09 +0200

    fix: list item key error

--- components/documents/stats-element-placeholder.tsx
+++ components/documents/stats-element-placeholder.tsx
@@ -1,10 +1,10 @@
 import { classNames } from "@/lib/utils";
 
-export default function StatsElementPlaceholder({ key }: { key: number }) {
+export default function StatsElementPlaceholder({ statIdx }: { statIdx: number }) {
   return (
     <div
       className={classNames(
-        key % 2 === 1 ? "sm:border-l" : key === 2 ? "lg:border-l" : "",
+        statIdx % 2 === 1 ? "sm:border-l" : statIdx === 2 ? "lg:border-l" : "",
         "border-t border-white/5 py-6 px-4 sm:px-6 lg:px-8"
       )}
     >
