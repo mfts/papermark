b252ca102b8b1691050ed907cc83298303ed0a84
Marc Seitz <marc@mfts.io>
2023-07-04 00:19:31 +0200

    rebuild vercel user-agent parser

---
+++ lib/utils/user-agent.ts
@@ -0,0 +1,42 @@
+import { UAParser } from "ua-parser-js";
+
+
+function isBot(input: string) {
+  return /Googlebot|Mediapartners-Google|AdsBot-Google|googleweblight|Storebot-Google|Google-PageRenderer|Bingbot|BingPreview|Slurp|DuckDuckBot|baiduspider|yandex|sogou|LinkedInBot|bitlybot|tumblr|vkShare|quora link preview|facebookexternalhit|facebookcatalog|Twitterbot|applebot|redditbot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview|ia_archiver/i.test(
+    input
+  );
+}
+
+export function userAgentFromString(input: string | undefined): UserAgent {
+  return {
+    ...new UAParser(input).getResult(),
+    isBot: input === undefined ? false : isBot(input),
+  };
+}
+
+
+
+interface UserAgent {
+  isBot: boolean;
+  ua: string;
+  browser: {
+    name?: string;
+    version?: string;
+  };
+  device: {
+    model?: string;
+    type?: string;
+    vendor?: string;
+  };
+  engine: {
+    name?: string;
+    version?: string;
+  };
+  os: {
+    name?: string;
+    version?: string;
+  };
+  cpu: {
+    architecture?: string;
+  };
+}
