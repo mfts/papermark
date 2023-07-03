93a9238c0529c201352717cf5641bdd40e4e8d9c
Marc Seitz <marc@mfts.io>
2023-07-04 00:20:46 +0200

    feat: add api route to record page view in tinybird

---
+++ lib/id-helper.ts
@@ -0,0 +1,37 @@
+import baseX from "base-x";
+
+function encodeBase58(buf: Buffer): string {
+  const alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+  return baseX(alphabet).encode(buf);
+}
+/**
+ * Generate ids similar to stripe
+ */
+export class IdGenerator<TPrefixes extends string> {
+  private prefixes: Record<TPrefixes, string>;
+
+  /**
+   * Create a new id generator with fully typed prefixes
+   * @param prefixes - Relevant prefixes for your domain
+   */
+  constructor(prefixes: Record<TPrefixes, string>) {
+    this.prefixes = prefixes;
+  }
+
+  /**
+   * Generate a new unique base58 encoded uuid with a defined prefix
+   *
+   * @returns xxxxxx_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+   */
+  public id = (prefix: TPrefixes): string => {
+    return [
+      this.prefixes[prefix],
+      encodeBase58(Buffer.from(crypto.randomUUID().replace(/-/g, ""), "hex")),
+    ].join("_");
+  };
+}
+
+export const newId = new IdGenerator({
+  view: "view",
+}).id;
---
+++ pages/api/record_view.ts
@@ -0,0 +1,109 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { newId } from "@/lib/id-helper";
+import { publishPageView } from "@/lib/tinybird";
+import { capitalize, getDomainWithoutWWW, log } from "@/lib/utils";
+import { userAgentFromString } from "@/lib/utils/user-agent";
+import { Geo } from "@/lib/types";
+import { LOCALHOST_GEO_DATA, getGeoData } from "@/lib/utils/geo";
+
+import { z } from "zod";
+
+
+const bodyValidation = z.object({
+  id: z.string(),
+  linkId: z.string(),
+  documentId: z.string(),
+  viewId: z.string(),
+  time: z.number().int(),
+  duration: z.number().int(),
+  pageNumber: z.string(),
+  country: z.string().optional(),
+  city: z.string().optional(),
+  region: z.string().optional(),
+  latitude: z.string().optional(),
+  longitude: z.string().optional(),
+  ua: z.string().optional(),
+  browser: z.string().optional(),
+  browser_version: z.string().optional(),
+  engine: z.string().optional(),
+  engine_version: z.string().optional(),
+  os: z.string().optional(),
+  os_version: z.string().optional(),
+  device: z.string().optional(),
+  device_vendor: z.string().optional(),
+  device_model: z.string().optional(),
+  cpu_architecture: z.string().optional(),
+  bot: z.boolean().optional(),
+  referer: z.string().optional(),
+  referer_url: z.string().optional(),
+});
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // We only allow POST requests
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method Not Allowed" });
+    return;
+  }
+
+  const geo: Geo =
+    process.env.VERCEL === "1" ? getGeoData(req.headers) : LOCALHOST_GEO_DATA;
+
+
+  const referer = req.headers.referer;
+  const ua = userAgentFromString(req.headers["user-agent"]);
+
+  const { linkId, documentId, viewId, duration, pageNumber } = req.body;
+
+  const time = Date.now(); // in milliseconds
+
+  const pageViewId = newId("view");
+
+
+  const pageViewObject = {
+    id: pageViewId,
+    linkId,
+    documentId,
+    viewId,
+    time,
+    duration,
+    pageNumber: pageNumber.toString(),
+    country: geo?.country || "Unknown",
+    city: geo?.city || "Unknown",
+    region: geo?.region || "Unknown",
+    latitude: geo?.latitude || "Unknown",
+    longitude: geo?.longitude || "Unknown",
+    ua: ua.ua || "Unknown",
+    browser: ua.browser.name || "Unknown",
+    browser_version: ua.browser.version || "Unknown",
+    engine: ua.engine.name || "Unknown",
+    engine_version: ua.engine.version || "Unknown",
+    os: ua.os.name || "Unknown",
+    os_version: ua.os.version || "Unknown",
+    device: ua.device.type ? capitalize(ua.device.type) : "Desktop",
+    device_vendor: ua.device.vendor || "Unknown",
+    device_model: ua.device.model || "Unknown",
+    cpu_architecture: ua.cpu?.architecture || "Unknown",
+    bot: ua.isBot,
+    referer: referer ? getDomainWithoutWWW(referer) : "(direct)",
+    referer_url: referer || "(direct)",
+  };
+
+  const result = bodyValidation.safeParse(pageViewObject);
+  if (!result.success) {
+    return res.status(400).json(
+      { error: `Invalid body: ${result.error.message}` }
+    );
+  }
+
+  try {
+    await publishPageView(result.data);
+
+    res.status(200).json({ message: "View recorded" });
+  } catch (error) {
+    log(`Failed to record view for ${linkId}. Error: \n\n ${error}`);
+    res.status(500).json({ message: (error as Error).message });
+  }
+}
