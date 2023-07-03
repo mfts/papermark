99751d1878cd88a5ec82969a28ecbca19107713b
Marc Seitz <marc@mfts.io>
2023-07-04 00:19:56 +0200

    rebuild vercel request geo header

---
+++ lib/utils/geo.ts
@@ -0,0 +1,32 @@
+import { Geo } from "../types";
+
+export function getGeoData(headers: {
+  [key: string]: string | string[] | undefined;
+}): Geo {
+  return {
+    city: Array.isArray(headers["x-vercel-ip-city"])
+      ? headers["x-vercel-ip-city"][0]
+      : headers["x-vercel-ip-city"],
+    region: Array.isArray(headers["x-vercel-ip-region"])
+      ? headers["x-vercel-ip-region"][0]
+      : headers["x-vercel-ip-region"],
+    country: Array.isArray(headers["x-vercel-ip-country"])
+      ? headers["x-vercel-ip-country"][0]
+      : headers["x-vercel-ip-country"],
+    latitude: Array.isArray(headers["x-vercel-ip-latitude"])
+      ? headers["x-vercel-ip-latitude"][0]
+      : headers["x-vercel-ip-latitude"],
+    longitude: Array.isArray(headers["x-vercel-ip-longitude"])
+      ? headers["x-vercel-ip-longitude"][0]
+      : headers["x-vercel-ip-longitude"],
+  };
+} 
+
+
+export const LOCALHOST_GEO_DATA = {
+  city: "Munich",
+  region: "BY",
+  country: "DE",
+  latitude: "48.137154",
+  longitude: "11.576124",
+};
