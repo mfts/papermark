4eaabb19c2869583c587e889f8db10acb28547a5
Marc Seitz <marc@mfts.io>
2023-07-24 01:45:37 +0200

    chore: add suppresshydrationwarning

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="h-full bg-black">
+    <Html lang="en" className="h-full bg-black" suppressHydrationWarning>
       <Head>
         <meta name="theme-color" content="#000000" />
         <meta
