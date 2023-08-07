de7a675524ca1a9a93412287505d821583da4fe4
Marc Seitz <marc@mfts.io>
2023-08-08 00:08:45 +0300

    fix: remove warnings for svg

--- components/shared/icons/bar-chart.tsx
+++ components/shared/icons/bar-chart.tsx
@@ -6,9 +6,9 @@ export default function BarChart({ className }: { className?: string }) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={className}
       shapeRendering="geometricPrecision"
     >
--- components/shared/icons/eye.tsx
+++ components/shared/icons/eye.tsx
@@ -6,9 +6,9 @@ export default function Eye({ className }: { className?: string }) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       shapeRendering="geometricPrecision"
       className={className}
     >
