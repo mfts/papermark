2da7a4c8277b2f11b71db6975c23b40f6fb1f5c8
Marc Seitz <marc@mfts.io>
2023-08-31 19:15:08 +0200

    feat: light up sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,7 @@ import {
 } from "@heroicons/react/20/solid";
 import { FolderIcon, HomeIcon, XMarkIcon, ChartBarIcon  } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { classNames } from "@/lib/utils";
+import { classNames, cn } from "@/lib/utils";
 import { useRouter } from "next/router";
 
 export default function Sidebar() {
@@ -21,7 +21,7 @@ export default function Sidebar() {
       href: "/overview",
       icon: HomeIcon,
       current: router.pathname.includes("overview"),
-      disabled: true,
+      disabled: false,
     },
     {
       name: "Documents",
@@ -56,7 +56,7 @@ export default function Sidebar() {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-gray-900/80" />
+            <div className="fixed inset-0 bg-background/80" />
           </Transition.Child>
 
           <div className="fixed inset-0 flex">
@@ -87,16 +87,16 @@ export default function Sidebar() {
                     >
                       <span className="sr-only">Close sidebar</span>
                       <XMarkIcon
-                        className="h-6 w-6 text-white"
+                        className="h-6 w-6 text-foreground"
                         aria-hidden="true"
                       />
                     </button>
                   </div>
                 </Transition.Child>
-                {/* Sidebar component, swap this element with another sidebar if you like */}
-                <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 ring-1 ring-white/10">
+                {/* Sidebar for mobile component, swap this element with another sidebar if you like */}
+                <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white dark:bg-black px-6 ring-1 ring-foreground/10">
                   <div className="flex h-16 shrink-0 items-center">
-                    <p className="text-2xl font-bold tracking-tighter text-white">
+                    <p className="text-2xl font-bold tracking-tighter text-black dark:text-white">
                       Papermark
                     </p>
                   </div>
@@ -108,16 +108,16 @@ export default function Sidebar() {
                             <li key={item.name}>
                               <button
                                 onClick={() => router.push(item.href)}
-                                className={classNames(
+                                className={cn(
                                   item.current
-                                    ? "bg-gray-800 text-white"
-                                    : "text-gray-400 hover:text-white hover:bg-gray-800",
-                                  "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold w-full disabled:hover:bg-inherit disabled:text-gray-400 disabled:cursor-default"
+                                    ? "bg-secondary text-secondary-foreground font-semibold"
+                                    : "text-muted-foreground hover:text-foreground hover:bg-muted",
+                                  "group flex gap-x-3 items-center rounded-md p-2 text-sm leading-6 w-full disabled:hover:bg-inherit disabled:text-muted-foreground disabled:cursor-default"
                                 )}
                                 disabled={item.disabled}
                               >
                                 <item.icon
-                                  className="h-6 w-6 shrink-0"
+                                  className="h-5 w-5 shrink-0"
                                   aria-hidden="true"
                                 />
                                 {item.name}
@@ -138,9 +138,9 @@ export default function Sidebar() {
       {/* Static sidebar for desktop */}
       <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
         {/* Sidebar component, swap this element with another sidebar if you like */}
-        <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-black/10 px-6">
+        <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white dark:bg-black px-6">
           <div className="flex h-16 shrink-0 items-center">
-            <p className="text-2xl font-bold tracking-tighter text-white">
+            <p className="text-2xl font-bold tracking-tighter text-black dark:text-white">
               Papermark
             </p>
           </div>
@@ -152,11 +152,11 @@ export default function Sidebar() {
                     <li key={item.name}>
                       <button
                         onClick={() => router.push(item.href)}
-                        className={classNames(
+                        className={cn(
                           item.current
-                            ? "bg-gray-800 text-white font-semibold"
-                            : "text-gray-400 hover:text-white hover:bg-gray-800",
-                          "group flex gap-x-3 items-center rounded-md p-2 text-sm leading-6 w-full disabled:hover:bg-inherit disabled:text-gray-400 disabled:cursor-default"
+                            ? "bg-secondary text-secondary-foreground font-semibold"
+                            : "text-muted-foreground hover:text-foreground hover:bg-muted",
+                          "group flex gap-x-3 items-center rounded-md p-2 text-sm leading-6 w-full disabled:hover:bg-inherit disabled:text-muted-foreground disabled:cursor-default"
                         )}
                         disabled={item.disabled}
                       >
@@ -172,9 +172,9 @@ export default function Sidebar() {
               </li>
               <li className="-mx-2 mt-auto mb-4">
                 <Menu as="div" className="relative">
-                  <Menu.Button className="flex items-center group rounded-md gap-x-3 p-2 w-full text-sm font-semibold leading-6 text-white hover:bg-gray-800">
+                  <Menu.Button className="flex items-center group rounded-md gap-x-3 p-2 w-full text-sm font-semibold leading-6 text-foreground hover:bg-secondary">
                     <img
-                      className="h-8 w-8 rounded-full bg-gray-800"
+                      className="h-8 w-8 rounded-full bg-secondary"
                       src={session?.user?.image || ""}
                       alt={`Profile picture of ${session?.user?.name}`}
                     />
@@ -182,7 +182,7 @@ export default function Sidebar() {
                       <span className="sr-only">Your profile</span>
                       <span aria-hidden="true">{session?.user?.name}</span>
                       <ChevronUpIcon
-                        className="ml-2 h-5 w-5 text-gray-400"
+                        className="ml-2 h-5 w-5 text-muted-foreground"
                         aria-hidden="true"
                       />
                     </span>
@@ -196,28 +196,28 @@ export default function Sidebar() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute left-0 z-10 bottom-0 mb-14 w-full origin-bottom-left rounded-md bg-gray-900 py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                      {session && (
-                        <Menu.Item>
-                          <p className="block px-3 py-1 text-sm leading-6 text-gray-400">
-                            {session?.user?.email}
-                          </p>
-                        </Menu.Item>
-                      )}
+                    <Menu.Items className="absolute left-0 z-10 bottom-0 mb-14 w-full origin-bottom-left rounded-md bg-primary-foreground py-2 ring-1 ring-primary-foreground/5 focus:outline-none">
                       {session ? (
-                        <Menu.Item>
-                          <Link
-                            onClick={() =>
-                              signOut({
-                                callbackUrl: `${window.location.origin}`,
-                              })
-                            }
-                            className="block px-3 py-1 text-sm leading-6 text-white hover:bg-gray-700"
-                            href={""}
-                          >
-                            Sign Out
-                          </Link>
-                        </Menu.Item>
+                        <>
+                          <Menu.Item>
+                            <p className="block px-3 py-1 text-sm leading-6 text-muted-foreground">
+                              {session?.user?.email}
+                            </p>
+                          </Menu.Item>
+                          <Menu.Item>
+                            <Link
+                              onClick={() =>
+                                signOut({
+                                  callbackUrl: `${window.location.origin}`,
+                                })
+                              }
+                              className="block px-3 py-1 text-sm leading-6 text-foreground hover:bg-muted"
+                              href={""}
+                            >
+                              Sign Out
+                            </Link>
+                          </Menu.Item>
+                        </>
                       ) : null}
                     </Menu.Items>
                   </Transition>
@@ -230,21 +230,15 @@ export default function Sidebar() {
 
       <div className="lg:pl-72">
         {/* Navbar */}
-        <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-black/10 bg-black/95 px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8 lg:hidden">
+        <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-white/90 bg-white dark:border-black/10 dark:bg-black/95 px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8 lg:hidden">
           <button
             type="button"
-            className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+            className="-m-2.5 p-2.5 text-muted-foreground lg:hidden"
             onClick={() => setSidebarOpen(true)}
           >
             <span className="sr-only">Open sidebar</span>
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
-
-          {/* Separator */}
-          <div
-            className="h-6 w-px bg-gray-900/10 lg:hidden"
-            aria-hidden="true"
-          />
 
           <div className="flex flex-1 gap-x-4 self-stretch items-center lg:gap-x-6 justify-end">
             <div className="flex items-center gap-x-4 lg:gap-x-6">
@@ -253,7 +247,7 @@ export default function Sidebar() {
                 <Menu.Button className="-m-1.5 flex items-center p-1.5">
                   <span className="sr-only">Open user menu</span>
                   <img
-                    className="h-8 w-8 rounded-full bg-gray-50"
+                    className="h-8 w-8 rounded-full bg-secondary"
                     src={session?.user?.image || ""}
                     alt={`Profile picture of ${session?.user?.name}`}
                   />
@@ -274,28 +268,28 @@ export default function Sidebar() {
                   leaveFrom="transform opacity-100 scale-100"
                   leaveTo="transform opacity-0 scale-95"
                 >
-                  <Menu.Items className="absolute right-0 z-10 mt-2.5 w-fit origin-top-right rounded-md bg-gray-900 py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                    {session && (
-                      <Menu.Item>
-                        <p className="block px-3 py-1 text-sm leading-6 text-gray-400">
-                          {session?.user?.email}
-                        </p>
-                      </Menu.Item>
-                    )}
+                  <Menu.Items className="absolute right-0 z-10 mt-2.5 w-fit origin-top-right rounded-md bg-primary-foreground shadow-lg py-2 ring-1 ring-primary-foreground/5 focus:outline-none">
                     {session ? (
-                      <Menu.Item>
-                        <Link
-                          onClick={() =>
-                            signOut({
-                              callbackUrl: `${window.location.origin}`,
-                            })
-                          }
-                          className="block px-3 py-1 text-sm leading-6 text-white hover:bg-gray-700"
-                          href={""}
-                        >
-                          Sign Out
-                        </Link>
-                      </Menu.Item>
+                      <>
+                        <Menu.Item>
+                          <p className="block px-3 py-1 text-sm leading-6 text-muted-foreground">
+                            {session?.user?.email}
+                          </p>
+                        </Menu.Item>
+                        <Menu.Item>
+                          <Link
+                            onClick={() =>
+                              signOut({
+                                callbackUrl: `${window.location.origin}`,
+                              })
+                            }
+                            className="block px-3 py-1 text-sm leading-6 text-foreground hover:bg-muted"
+                            href={""}
+                          >
+                            Sign Out
+                          </Link>
+                        </Menu.Item>
+                      </>
                     ) : null}
                   </Menu.Items>
                 </Transition>
