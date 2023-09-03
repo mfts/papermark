0608137a62ab6a7a376508f9de983045a96eb523
Marc Seitz <marc@mfts.io>
2023-09-03 23:26:03 +0200

    feat: add image domains to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["darr6llpvnyot.cloudfront.net", "lh3.googleusercontent.com"],
+  },
+};
 
 module.exports = nextConfig
