94fc03003262806cf0d1e04de3934fa0204510e0
Marc Seitz <marc@mfts.io>
2023-09-05 18:13:36 +0200

    feat: first app router file ✨

---
+++ app/sitemap.ts
@@ -0,0 +1,38 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.papermark.io",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.papermark.io/alternatives/docsend",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.papermark.io/alternatives/brieflink",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.papermark.io/alternatives/pandadoc",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.papermark.io/alternatives/google-drive",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.papermark.io/alternatives/pitch",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.papermark.io/privacy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://vc.papermark.io",
+      lastModified: new Date(),
+    },
+  ];
+}
