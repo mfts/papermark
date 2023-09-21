fb43f4953894b9eb7ae2816489a8b9e42a1f2bcb
Marc Seitz <marc@mfts.io>
2023-09-21 22:03:02 +0200

    feat: add oss-friends page

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
