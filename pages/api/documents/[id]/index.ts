d66b196e1698967f00d14dde90903fb1181a0d80
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-09-23 21:13:10 +0545

    feat: detele document

--- pages/api/documents/[id]/index.ts
+++ pages/api/documents/[id]/index.ts
@@ -40,9 +40,51 @@ export default async function handle(
         error: (error as Error).message,
       });
     }
+  } else if (req.method === "DELETE") {
+    // DELETE /api/document/:id
+    const session = await getServerSession(req, res, authOptions);
+    if (!session) { 
+      res.status(401).end("Unauthorized");
+      return;
+    }
+
+    const { id } = req.query as { id: string };
+
+    try{
+      const document = await prisma.document.findUnique({
+        where: {
+          id: id,
+        },
+      });
+      
+      if (!document) {
+        return res.status(404).end("Document not found");
+      }
+      
+      // check that the user is owner of the document, otherwise return 401
+      if (document.ownerId !== (session.user as CustomUser).id) {
+        return res.status(401).end("Unauthorized to access the document");
+      }
+  
+      // delete the document 
+      await prisma.document.delete({
+        where: {
+          id: id,
+        }
+      })
+      
+      // successfully deleted
+      return res.status(200).json("Document deleted successfully");
+    } catch (error) {
+      return res.status(500).json({
+        message: "Internal Server Error",
+        error: (error as Error).message,
+      });
+    }
+
   } else {
-    // We only allow GET and POST requests
-    res.setHeader("Allow", ["GET"]);
+    // We only allow GET, DELETE and POST requests
+    res.setHeader("Allow", ["GET", "DELETE"]);
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
