7fbb260a7978d80e2621dd15d7e606803f83f12a
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-10-04 19:32:40 +0545

    team modal

--- components/teams/add-team-modal.tsx
+++ components/teams/add-team-modal.tsx
@@ -10,15 +10,20 @@ import {
   DialogTrigger
 } from "@/components/ui/dialog";
 import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
 
 
 export function AddTeamModal({children}: {children: React.ReactNode}) {
   const router = useRouter();
   const plausible = usePlausible();
-  const [teamName, setTeamName] = useState<String>("");
+  const [teamName, setTeamName] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleCreateTeam = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    // TODO: handle team creation
   }
 
   return (
@@ -28,19 +33,31 @@ export function AddTeamModal({children}: {children: React.ReactNode}) {
           <DialogHeader>
             <DialogTitle>Create a Team</DialogTitle>
             <DialogDescription>
-            <div className="border-b border-border py-2">
+            <div className="py-2">
               <p className="mb-1 text-sm text-muted-foreground">
                 Start by naming your new team and inviting team members.
               </p>
             </div>
             <form 
               onSubmit={handleCreateTeam} 
-              className="flex flex-col"
-            >
+              className="flex flex-col gap-8 mt-4"
+            >   
+              <div className="flex flex-col gap-3">
+                <div>
+                  <Label>Team name</Label>
+                </div>
+                <div>
+                  <Input 
+                    placeholder="Enter team's name"
+                    onChange={(e) => setTeamName(e.target.value)} 
+                  />
+                </div>
+              </div>
               <div className="flex justify-center">
                 <Button
                   type="submit"
                   className="w-full lg:w-1/2"
+                  loading={loading}
                   // disabled={uploading || !currentFile}
                 >
                   Create
