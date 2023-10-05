d4b5c0b2aef559db8a1939b96f17a70892256cde
Marc Seitz <marc@mfts.io>
2023-10-05 16:04:57 +0200

    fix: caching of investor data

--- app/(static)/open-source-investors/page.tsx
+++ app/(static)/open-source-investors/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import Dashboard from "./ClientPage";
 import { cache } from "react";
 
-export const revalidate = 86400; // revalidate the data at most every 24 hours
+export const revalidate = 3600; // revalidate the data at most every 24 hours
 
 const data = {
   description:
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
   },
 };
 
-const getInvestors = cache(async () => {
+const getInvestors = async () => {
   const response = await fetch(
     "https://api.airtable.com/v0/appAxYMTCbZ1hGTmg/tblvBs5aqTt8qkb6h?fields%5B%5D=name&fields%5B%5D=title&fields%5B%5D=company&fields%5B%5D=checkSize&fields%5B%5D=openSourceInvestments&fields%5B%5D=twitterUrl&fields%5B%5D=websiteUrl&fields%5B%5D=twitterImageUrl&filterByFormula=AND(%7Bpublished%7D%3D1%2C%7Btype%7D%3D%22Angel%22)",
     {
@@ -52,7 +52,7 @@ const getInvestors = cache(async () => {
   }
   const data = await response.json();
   return data;
-});
+};
 
 export default async function HomePage() {
   const data = await getInvestors();
