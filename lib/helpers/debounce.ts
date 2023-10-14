69b70c7a288caf6b14645bc13c804d30e7782c87
Raju-github-profile <rajukadel.github@gmail.com>
2023-10-14 22:27:09 +0545

    added debouncing for rapid eventclicks

---
+++ lib/helpers/debounce.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+export function useDebounce<T extends (...args: any[]) => void>(
+    func: T,
+    delay: number
+): (...args: Parameters<T>) => void {
+    const [timeoutId, setTimeoutId] = useState<number | null>(null);
+
+    useEffect(() => {
+        return () => {
+
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+        };
+    }, [timeoutId]);
+
+    return function (...args: Parameters<T>) {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+
+        const context = this;
+
+        const newTimeoutId = setTimeout(() => {
+            func.apply(context, args);
+        }, delay);
+
+        setTimeoutId(newTimeoutId);
+    };
+}
