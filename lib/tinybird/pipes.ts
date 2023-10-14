6dc59b1f3f263b4246c965b92ec1b135bc78588d
Marc Seitz <marc@mfts.io>
2023-10-14 21:50:26 +0200

    update tinybird pipes and datasource

--- lib/tinybird/pipes.ts
+++ lib/tinybird/pipes.ts
@@ -5,19 +5,20 @@ import { Tinybird } from "@chronark/zod-bird";
 const tb = new Tinybird({ token: process.env.TINYBIRD_TOKEN! });
 
 export const getTotalAvgPageDuration = tb.buildPipe({
-  pipe: "get_total_average_page_duration__v1",
+  pipe: "get_total_average_page_duration__v2",
   parameters: z.object({
     documentId: z.string(),
     since: z.number(),
   }),
   data: z.object({
+    versionNumber: z.number().int(),
     pageNumber: z.string(),
     avg_duration: z.number(),
   }),
 });
 
 export const getViewPageDuration = tb.buildPipe({
-  pipe: "get_page_duration_per_view__v2",
+  pipe: "get_page_duration_per_view__v3",
   parameters: z.object({
     documentId: z.string(),
     viewId: z.string(),
