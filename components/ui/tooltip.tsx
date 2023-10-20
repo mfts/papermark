eaf834170c1f0f736530d4166f1405f26d1162b5
Raju-github-profile <rajukadel.github@gmail.com>
2023-10-20 18:45:57 +0545

    added tooltip

---
+++ components/ui/tooltip.tsx
@@ -0,0 +1,33 @@
+import React, { MouseEventHandler, ReactNode } from 'react'
+import * as Tooltip from "@radix-ui/react-tooltip";
+import Eye from '../shared/icons/eye';
+
+interface TooltipProps {
+    title: string;
+    children: ReactNode
+}
+
+function TooltipBox({ title, children }: TooltipProps) {
+    return (
+        <Tooltip.Provider>
+            <Tooltip.Root delayDuration={0}>
+
+                <Tooltip.Trigger asChild>
+                    {children}
+                </Tooltip.Trigger>
+
+                <Tooltip.Portal>
+                    <Tooltip.Content
+                        className="TooltipContent p-1 px-2 font-light  bg-slate-800 text-slate-200"
+                        sideOffset={5}>
+                        {title}
+                        <Tooltip.Arrow className="TooltipArrow" />
+                    </Tooltip.Content>
+                </Tooltip.Portal>
+
+            </Tooltip.Root>
+        </Tooltip.Provider>
+    )
+}
+
+export {TooltipBox}
