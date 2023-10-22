d86e3a571ab640f97459e22897820a6edaf150a3
Marc Seitz <marc@mfts.io>
2023-10-22 18:12:46 +0200

    feat: add convert pdf function

---
+++ pages/api/mupdf/convert-page.ts
@@ -0,0 +1,57 @@
+import { NextApiRequest, NextApiResponse } from "next";
+// @ts-ignore
+import mupdf from "mupdf";
+import { put } from "@vercel/blob";
+import prisma from "@/lib/prisma";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // check if post method
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Method Not Allowed" });
+    return;
+  }
+
+  try {
+    const { documentVersionId, pageNumber, url } = req.body as {
+      documentVersionId: string;
+      pageNumber: number;
+      url: string; 
+    };
+
+    // Fetch the PDF data
+    const response = await fetch(url);
+    // Convert the response to an ArrayBuffer
+    const pdfData = await response.arrayBuffer();
+    // Create a MuPDF instance
+    var doc = mupdf.Document.openDocument(pdfData, "application/pdf");
+
+    var page = doc.loadPage(pageNumber-1); // 0-based page index
+    var pixmap = page.toPixmap(
+      mupdf.Matrix.identity,
+      mupdf.ColorSpace.DeviceRGB
+    );
+    var pngBuffer = pixmap.asPNG();
+    const blob = await put(`page-${pageNumber}.png`, pngBuffer, {
+      access: "public",
+    });
+
+    const documentPage = await prisma.documentPage.create({
+      data: {
+        versionId: documentVersionId,
+        pageNumber: pageNumber,
+        file: blob.url,
+      },
+    });
+
+    if (!documentPage) {
+      res.status(500).json({ error: "Failed to create document page" });
+      return;
+    }
+
+    // Send the images as a response
+    res.status(200).json({ documentPageId: documentPage.id });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
