56eeb95ef6a394486652d2f26da995e704e7ff90
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-10-29 21:24:48 +0545

    api: delete team

--- pages/api/teams/[teamId]/index.ts
+++ pages/api/teams/[teamId]/index.ts
@@ -65,5 +65,56 @@ export default async function handle(
     } catch (error) {
       errorHanlder(error, res);
     }
+  } else if (req.method === "DELETE") {
+    // DELETE /api/teams/:teamId
+    const session = await getServerSession(req, res, authOptions);
+    if (!session) {
+      res.status(401).end("Unauthorized");
+      return;
+    }
+
+    const { teamId } = req.query as { teamId: string };
+
+    try {
+      // check if the team exists
+      const team = await prisma.team.findUnique({
+        where: {
+          id: teamId,
+        },
+        include: {
+          users: true,
+        },
+      });
+      if (!team) {
+        return res.status(400).json("Team doesn't exists");
+      }
+
+      // check if current user is admin of the team
+      const isUserAdmin = team.users.some(
+        (user) =>
+          user.role === "ADMIN" &&
+          user.userId === (session.user as CustomUser).id
+      );
+      if (!isUserAdmin) {
+        return res
+          .status(403)
+          .json("You are not permitted to perform this action");
+      }
+
+      // delete team
+      await prisma.team.delete({
+        where: {
+          id: teamId,
+        },
+      });
+
+      return res.status(204).end();
+    } catch (error) {
+      return res.status(500).json({ message: (error as Error).message });
+    }
+  } else {
+    // We only allow GET and DELETE requests
+    res.setHeader("Allow", ["GET", "DELETE"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
