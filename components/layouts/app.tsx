4735794e441cde8aab532c6e5a5081efac6ac69f
Kinfe123 <kinfishtech@gmail.com>
2023-11-05 18:19:55 +0300

    fix: gap fix

--- components/layouts/app.tsx
+++ components/layouts/app.tsx
@@ -4,7 +4,7 @@ import Sidebar from "../Sidebar";
 export default function AppLayout({children}: {children: React.ReactNode}) {
   return (
     <div>
-      <div className="flex gap-4 flex-col lg:flex-row min-h-screen bg-gray-50 dark:bg-black">
+      <div className="flex gap-7 flex-col lg:flex-row min-h-screen bg-gray-50 dark:bg-black">
         <Sidebar />
         <main className=" lg:m-2 grow w-full bg-white dark:bg-gray-900 rounded-xl ring-1 ring-gray-200 dark:ring-gray-800">
           {children}
