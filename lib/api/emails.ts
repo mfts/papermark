b817f39dde6057a5f42084c820e9b74a504429a4
Digant Patel <digantpatel404@gmail.com>
2023-11-10 21:55:42 +0530

    Send Document and Invite Recipient Features Done

--- lib/api/emails.ts
+++ lib/api/emails.ts
@@ -1,3 +1,5 @@
+import prisma from "../prisma";
+
 export async function fetchBlobFile(blobURL: string) {
   try {
     const response = await fetch(blobURL);
@@ -9,4 +11,32 @@ export async function fetchBlobFile(blobURL: string) {
     console.error('Error fetching the blob file:', error);
     return null;
   }
+}
+
+export async function generateAuthenticationCodeURL(email: string, linkId: string, invited: boolean, documentLink?: string) {
+  const authenticationCode = generateUniqueString(12);
+  await prisma.emailAuthenticationCode.create({
+    data: {
+      email,
+      code: authenticationCode,
+      linkId,
+      invited
+    }
+  }) 
+  const URL = documentLink
+   ? `${documentLink}?authenticationCode=${authenticationCode}` 
+   : `${process.env.NEXTAUTH_URL}/view/${linkId}?authenticationCode=${authenticationCode}`
+  return URL;
+}
+
+function generateUniqueString(length: number) {
+  const charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; // You can customize this as needed.
+  let uniqueString = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    uniqueString += charset.charAt(randomIndex);
+  }
+
+  return uniqueString;
 }
