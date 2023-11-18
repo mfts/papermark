545b9bc38f00d7f3a21f9a852b97c46b9d28bf63
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-11-18 14:36:47 +0545

    feat: api endpoint for creating and geting webhooks

---
+++ pages/api/webhooks/index.ts
@@ -0,0 +1,55 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+import { CustomUser } from "@/lib/types";
+import prisma from "@/lib/prisma";
+import { errorhandler } from "@/lib/errorHandler";
+
+export async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const session = await getServerSession(req, res, authOptions);
+    if (!session) {
+      return res.status(401).end("Unauthorized");
+    }
+
+    const userId = (session.user as CustomUser).id;
+
+    const { targetUrl, events } = req.body;
+
+    try {
+      const webhook = await prisma.webhook.create({
+        data: {
+          userId,
+          targetUrl,
+          events,
+        }
+      })
+
+      return res.status(201).json(webhook);
+    }catch (error) {
+      errorhandler(error, res);
+    }
+  }
+  else if (req.method === "GET") {
+    const session = await getServerSession(req, res, authOptions);
+    if (!session) {
+      return res.status(401).end("Unauthorized");
+    }
+
+    const userId = (session.user as CustomUser).id;
+
+    try {
+      const webhooks = await prisma.webhook.findMany({
+        where: {
+          userId,
+        }
+      })
+      return res.status(200).json(webhooks);
+    }catch (error) {
+      errorhandler(error, res);
+    }
+  } else {
+    res.setHeader("Allow", ["GET", "POST"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
