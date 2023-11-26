da1a65777378517b9e272f582e8f7f2bc37be1ef
Kinfe123 <kinfishtech@gmail.com>
2023-11-26 08:13:19 -0500

    fix: setting up google oatuh configs

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -19,6 +19,17 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+            prompt: "consent",
+            access_type: "offline",
+            response_type: "code",
+        },
+    },
+  
+    allowDangerousEmailAccountLinking: true,
+  
+
     }),
   ],
   adapter: PrismaAdapter(prisma),
