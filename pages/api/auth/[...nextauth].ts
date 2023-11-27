9fa52e7f354b7fdb98cd81be3c3332099b2ab437
Kinfe123 <kinfishtech@gmail.com>
2023-11-27 23:13:14 +0300

    fix: oauth fi

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -45,7 +45,7 @@ export const authOptions: NextAuthOptions = {
         sameSite: "lax",
         path: "/",
         // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
-        domain: VERCEL_DEPLOYMENT ? ".papermark.io" : undefined,
+        domain: VERCEL_DEPLOYMENT ? ".papermark.io" || ".papermark-kinfish.vercel.app" : undefined,
         secure: VERCEL_DEPLOYMENT,
       },
     },
