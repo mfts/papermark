641e509a39b40ed5d7beb7eec9e8897bc72eb0e6
Marc Seitz <marc@mfts.io>
2024-01-05 00:45:41 +1300

    chore: remove suggested edit

--- components/links/link-sheet/index.tsx
+++ components/links/link-sheet/index.tsx
@@ -200,7 +200,7 @@ export default function LinkSheet({
 
           <SheetFooter>
             <div className="flex items-center">
-              <Button type="submit" disabled={isLoading} loading={isLoading}>
+              <Button type="submit" loading={isLoading}>
                 {currentLink ? "Update Link" : "Save Link"}
               </Button>
             </div>
