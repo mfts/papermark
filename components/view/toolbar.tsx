6bd48a55cc6e6c24b988ea7a3c0179d84b79e57f
Marc Seitz <marc@mfts.io>
2024-01-05 19:29:53 +1300

    feat: make toolbar draggable

--- components/view/toolbar.tsx
+++ components/view/toolbar.tsx
@@ -2,7 +2,8 @@ import { useEffect, useRef, useState } from "react";
 import { Avatar, AvatarFallback } from "../ui/avatar";
 import UserRound from "../shared/icons/user-round";
 import { REACTIONS } from "@/lib/constants";
-import { set } from "date-fns";
+import GripVertical from "../shared/icons/grip-vertical";
+import Draggable from "react-draggable";
 
 function getKeyByValue(object: { [x: string]: any }, value: any) {
   return Object.keys(object).find((key) => object[key] === value);
@@ -33,7 +34,7 @@ export default function Toolbar({
     };
   }, []);
 
-  const handleEmojiClick = async (emoji: string) => {
+  const handleEmojiClick = async (emoji: string, label: string) => {
     // Clear any existing timeout
     if (clearEmojiTimeout.current) {
       clearTimeout(clearEmojiTimeout.current);
@@ -54,61 +55,60 @@ export default function Toolbar({
       },
       body: JSON.stringify({
         viewId: viewId,
-        type: getKeyByValue(REACTIONS, emoji) as string,
+        type: label,
         pageNumber: pageNumber,
       }),
     });
   };
 
-  const Emoji = (props: any) => (
-    <button
-      className="emoji text-2xl leading-6 hover:bg-gray-800 rounded-full p-2"
-      role="img"
-      aria-label={props.label ? props.label : ""}
-      aria-hidden={props.label ? "false" : "true"}
-      onClick={() => handleEmojiClick(props.symbol)}
-    >
-      {/* {(props.symbol === "👍" ||
-        props.symbol === "👎" ||
-        props.symbol === "❤️" ||
-        props.symbol === "💸") &&
-        flyEmojis === props.symbol && (
+  const Emoji = ({ label, emoji }: { label: string; emoji: string }) => (
+    <div className="relative w-fit">
+      <button
+        className="font-emoji text-2xl leading-6 bg-transparent p-1 relative transition-bg-color duration-600 inline-flex justify-center items-center align-middle rounded-full ease-in-out hover:bg-gray-200 active:bg-gray-400 active:duration-0"
+        role="img"
+        aria-label={label ? label : ""}
+        aria-hidden={label ? "false" : "true"}
+        onClick={() => handleEmojiClick(emoji, label)}
+      >
+        {emoji}
+        {currentEmoji && currentEmoji.emoji === emoji && (
           <span
-            className={`absolute -top-10 left-0 right-0 mx-auto animate-flyEmoji duration-3000`}
+            key={currentEmoji.id}
+            className="font-emoji absolute -top-10 left-0 right-0 mx-auto animate-flyEmoji duration-3000"
           >
-            {flyEmojis}
+            {currentEmoji.emoji}
           </span>
-        )} */}
-      {props.symbol}
-      {currentEmoji && currentEmoji.emoji === props.symbol && (
-        <span
-          key={currentEmoji.id}
-          className="absolute -top-10 left-0 right-0 mx-auto animate-flyEmoji duration-3000"
-        >
-          {currentEmoji.emoji}
-        </span>
-      )}
-    </button>
+        )}
+      </button>
+    </div>
   );
 
   return (
     <>
-      <div className="fixed bottom-10 left-1/2 transform -translate-x-1/2 bg-gray-950/50 rounded-full py-1 px-4 flex items-center justify-start space-x-2 shadow-lg z-10">
-        <div className="p-1 rounded-full bg-emerald-500 -ml-2">
-          <Avatar className="h-6 w-6">
-            {/* <AvatarImage src= /> */}
-            <AvatarFallback>
-              <UserRound />
-            </AvatarFallback>
-          </Avatar>
+      <Draggable
+        bounds="parent"
+        handle=".moveable-icon"
+        defaultClassName="mt-12"
+      >
+        <div className="fixed bottom-10 left-1/2 flex z-10">
+          <div className="bg-gray-950/50 border border-gray-900 rounded-full mx-auto mt-4 mb-4">
+            <div className="grid items-center justify-start">
+              <div className="p-2">
+                <div className="grid items-center justify-start grid-flow-col">
+                  {REACTIONS.map((reaction) => (
+                    <Emoji
+                      key={reaction.emoji}
+                      emoji={reaction.emoji}
+                      label={reaction.label}
+                    />
+                  ))}
+                  <GripVertical className="h-6 w-6 text-gray-100 active:text-gray-300 moveable-icon" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex items-center">
-          <Emoji symbol="❤️" label="heart" />
-          <Emoji symbol="💸" label="money" />
-          <Emoji symbol="👍" label="thumbs-up" />
-          <Emoji symbol="👎" label="thumbs-down" />
-        </div>
-      </div>
+      </Draggable>
     </>
   );
 }
