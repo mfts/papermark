e627bfcb195fcfd49fe5f26a9c393da4e5f9dff0
Marc Seitz <marc@mfts.io>
2024-01-29 14:29:23 +1100

    chore: merge investors to papermark

--- app/(static)/investors/page.tsx
+++ app/(static)/investors/page.tsx
@@ -5,9 +5,9 @@ export const revalidate = 3600; // revalidate the data at most every 24 hours
 
 const data = {
   description:
-    "List of 100 open-source investors. Open-source VC, open-source angel investors. Share pitchdecks with your investors using Papermark, an open-source document infrastructure.",
-  title: "Open Source Investors | Papermark",
-  url: "https://www.papermark.io",
+    "Find over 10,000 investors based on stage, sector, or location. Powered by Papermark.",
+  title: "Investor Search | Papermark",
+  url: "https://www.papermark.io/investors",
 };
 
 export const metadata: Metadata = {
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     siteName: "Papermark",
     images: [
       {
-        url: "https://www.papermark.io/_static/meta-image.png",
+        url: "https://www.papermark.io/_static/investor-meta.png",
         width: 800,
         height: 600,
       },
@@ -33,19 +33,18 @@ export const metadata: Metadata = {
     title: data.title,
     description: data.description,
     creator: "@papermarkio",
-    images: ["https://www.papermark.io/_static/meta-image.png"],
+    images: ["https://www.papermark.io/_static/investor-meta.png"],
   },
 };
 
 const getInvestors = async () => {
-  const response = await fetch(
-    "https://api.airtable.com/v0/appAxYMTCbZ1hGTmg/tblvBs5aqTt8qkb6h?fields%5B%5D=name&fields%5B%5D=title&fields%5B%5D=company&fields%5B%5D=checkSize&fields%5B%5D=openSourceInvestments&fields%5B%5D=twitterUrl&fields%5B%5D=websiteUrl&fields%5B%5D=twitterImageUrl&filterByFormula=AND(%7Bpublished%7D%3D1%2C%7Btype%7D%3D%22Angel%22)",
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
-      },
+  const response = await fetch(`https://investors.papermark.io/api/investors`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.INVESTORS_API_KEY}`,
     },
-  );
+  });
   if (!response.ok) {
     throw new Error("Network response was not ok " + response.statusText);
   }
@@ -55,6 +54,10 @@ const getInvestors = async () => {
 
 export default async function HomePage() {
   const data = await getInvestors();
+  // const data = [
+  //   {id: 1,
+  //   name: "test"}
+  // ]
 
   return <Dashboard data={data} />;
 }
