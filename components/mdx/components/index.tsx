33f65bd11fd6d936e0759491d44bb01dcf6aea03
Marc Seitz <marc@mfts.io>
2024-02-16 15:25:49 +1100

    style: add orange button to mdx components

--- components/mdx/components/index.tsx
+++ components/mdx/components/index.tsx
@@ -3,12 +3,15 @@ import { MDXImage } from "./mdx-image";
 import { MDXComponents } from "mdx/types";
 import { Code } from "bright";
 import NextImage from "next/image";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 export const mdxComponents: MDXComponents = {
   a: ({ children, ...props }) => {
     // check if external links
     let isExternal = false;
-    if (props.href?.startsWith("http")) {
+    // if the link is not from papermark.io, it's external
+    if (props.href?.startsWith("http") || !props.href?.includes("papermark.io")) {
       isExternal = true;
     }
 
@@ -16,8 +19,9 @@ export const mdxComponents: MDXComponents = {
       // @ts-expect-error legacy refs
       <Link
         {...props}
+        className={cn(props.className, "text-[#fb7a00] font-semibold no-underline hover:underline")}
         href={props.href || ""}
-        target={isExternal ? "_blank" : undefined}
+        target="_blank"
         rel={isExternal ? "noopener noreferrer" : undefined}
       >
         {children}
@@ -27,5 +31,28 @@ export const mdxComponents: MDXComponents = {
   img: MDXImage as any,
   Image: NextImage as any,
   pre: Code,
+  Button: ({ children, ...props }) => {
+    // check if external links
+    let isExternal = false;
+    // if the link is not from papermark.io, it's external
+    if (props.href?.startsWith("http") || !props.href?.includes("papermark.io")) {
+      isExternal = true;
+    }
+
+    return (
+      <Link
+        href={props.href || ""}
+        target="_blank"
+        rel={isExternal ? "noopener noreferrer" : undefined}
+      >
+        <Button
+          variant={props.variant || "default"}
+          className={props.className}
+        >
+          {children}
+        </Button>
+      </Link>
+    );
+  }
   // any other components you want to use in your markdown
 };
