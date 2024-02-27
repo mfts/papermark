42ff0793864adda0fdad65f55925ccf08a02f900
Marc Seitz <marc@mfts.io>
2024-02-27 15:55:54 +1100

    feat: add limits to certain link options

--- components/links/link-sheet/link-options.tsx
+++ components/links/link-sheet/link-options.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { DEFAULT_LINK_TYPE } from "@/components/links/link-sheet";
 
 import EmailProtectionSection from "@/components/links/link-sheet/email-protection-section";
@@ -17,6 +17,8 @@ import DenyListSection from "@/components/links/link-sheet/deny-list-section";
 import PasswordSection from "@/components/links/link-sheet/password-section";
 import OGSection from "@/components/links/link-sheet/og-section";
 import FeedbackSection from "@/components/links/link-sheet/feedback-section";
+import { UpgradePlanModal } from "@/components/billing/upgrade-plan-modal";
+import { usePlan } from "@/lib/swr/use-billing";
 
 export const LinkOptions = ({
   data,
@@ -25,12 +27,22 @@ export const LinkOptions = ({
   data: DEFAULT_LINK_TYPE;
   setData: Dispatch<SetStateAction<DEFAULT_LINK_TYPE>>;
 }) => {
+  const { plan } = usePlan();
+  const hasFreePlan = plan?.plan === "free";
+
+  const [openUpgradeModal, setOpenUpgradeModal] = useState<boolean>(false);
+
   return (
     <div>
       <EmailProtectionSection {...{ data, setData }} />
       <AllowNotificationSection {...{ data, setData }} />
       <AllowDownloadSection {...{ data, setData }} />
       <ExpirationSection {...{ data, setData }} />
+      <OGSection
+        {...{ data, setData }}
+        hasFreePlan={hasFreePlan}
+        setOpenUpgradeModal={setOpenUpgradeModal}
+      />
 
       <Accordion type="single" collapsible>
         <AccordionItem value="item-1" className="border-none">
@@ -40,15 +52,33 @@ export const LinkOptions = ({
             </span>
           </AccordionTrigger>
           <AccordionContent className="first:pt-5">
-            <EmailAuthenticationSection {...{ data, setData }} />
-            <AllowListSection {...{ data, setData }} />
-            <DenyListSection {...{ data, setData }} />
+            <EmailAuthenticationSection
+              {...{ data, setData }}
+              hasFreePlan={hasFreePlan}
+              setOpenUpgradeModal={setOpenUpgradeModal}
+            />
+            <AllowListSection
+              {...{ data, setData }}
+              hasFreePlan={hasFreePlan}
+              setOpenUpgradeModal={setOpenUpgradeModal}
+            />
+            <DenyListSection
+              {...{ data, setData }}
+              hasFreePlan={hasFreePlan}
+              setOpenUpgradeModal={setOpenUpgradeModal}
+            />
             <PasswordSection {...{ data, setData }} />
-            <OGSection {...{ data, setData }} />
             <FeedbackSection {...{ data, setData }} />
           </AccordionContent>
         </AccordionItem>
       </Accordion>
+      {hasFreePlan ? (
+        <UpgradePlanModal
+          clickedPlan="Pro"
+          open={openUpgradeModal}
+          setOpen={setOpenUpgradeModal}
+        />
+      ) : null}
     </div>
   );
 };
