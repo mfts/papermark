7d957155de3a8cb02a644bd6bba8a0b86a3a077e
Marc Seitz <marc@mfts.io>
2024-03-03 22:50:47 +1100

    feat: add server-side posthog client

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -1,3 +1,5 @@
+import { PostHog } from "posthog-node";
+
 export function getPostHogConfig(): { key: string; host: string } | null {
   const postHogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
   const postHogHost = `${process.env.NEXT_PUBLIC_BASE_URL}/ingest`;
@@ -10,4 +12,19 @@ export function getPostHogConfig(): { key: string; host: string } | null {
     key: postHogKey,
     host: postHogHost,
   };
+}
+
+export function getPostHogServerClient() {
+  const posthogConfig = getPostHogConfig();
+  const isPostHogEnabled = !!posthogConfig;
+
+  if (!isPostHogEnabled) {
+    return null;
+  }
+
+  const client = new PostHog(posthogConfig.key, {
+    host: posthogConfig.host,
+  });
+
+  return client;
 }
