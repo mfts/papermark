d29b1435662f74273f1abc93f74cec3c8cff4a18
Marc Seitz <marc@mfts.io>
2024-03-03 22:52:11 +1100

    feat: add analytics hooks (client and server)

--- lib/analytics/index.ts
+++ lib/analytics/index.ts
@@ -1,70 +1,142 @@
 import { emptyAnalytics, jitsuAnalytics } from "@jitsu/js";
-import { AnalyticsEvents } from "../types";
+import { AnalyticsEvents } from "@/lib/types";
 import prisma from "@/lib/prisma";
+import { getPostHogConfig, getPostHogServerClient } from "@/lib/posthog";
+import { posthog } from "posthog-js";
 
-export const analytics =
-  process.env.JITSU_HOST && process.env.JITSU_WRITE_KEY
-    ? jitsuAnalytics({
-        host: process.env.JITSU_HOST,
-        writeKey: process.env.JITSU_WRITE_KEY,
-      })
-    : emptyAnalytics;
+export function useAnalytics() {
+  const isPostHogEnabled = getPostHogConfig();
 
-const fetchUserWithCounts = async (userId: string) => {
-  return await prisma.user.findUnique({
-    where: { id: userId },
-    select: {
-      id: true,
-      email: true,
-      _count: {
-        select: {
-          documents: true,
-          domains: true,
-        },
-      },
-      documents: {
-        select: {
-          _count: {
-            select: {
-              links: true,
-              views: true,
-            },
-          },
-        },
-      },
-    },
-  });
-};
+  /**
+   * Capture an analytic event.
+   *
+   * @param event The event name.
+   * @param properties Properties to attach to the event.
+   */
+  const capture = (event: string, properties?: Record<string, unknown>) => {
+    if (!isPostHogEnabled) {
+      return;
+    }
 
-const getUserProperties = (user: any) => {
-  let linkCount = user.documents.reduce(
-    (acc: any, document: { _count: { links: number; views: number } }) =>
-      acc + document._count.links,
-    0,
-  );
-
-  let viewCount = user.documents.reduce(
-    (acc: any, document: { _count: { links: number; views: number } }) =>
-      acc + document._count.views,
-    0,
-  );
-
-  const userData = {
-    userId: user.id,
-    email: user.email,
-    documentCount: user._count.documents,
-    domainCount: user._count.domains,
-    linkCount: linkCount,
-    viewCount: viewCount,
+    posthog.capture(event, properties);
+    console.log("tracked pageview", event, properties);
   };
 
-  return userData;
-};
+  const identify = (
+    distinctId?: string,
+    properties?: Record<string, unknown>,
+  ) => {
+    if (!isPostHogEnabled) {
+      return;
+    }
 
-export const identifyUser = async (userId: string) => {
-  const user = await fetchUserWithCounts(userId);
-  const userProperties = getUserProperties(user);
-  analytics.identify(userId, userProperties);
-};
+    posthog.identify(distinctId, properties);
+    console.log("identified user", distinctId, properties);
+  };
 
-export const trackAnalytics = (args: AnalyticsEvents) => analytics.track(args);
+  return {
+    capture,
+    identify,
+  };
+}
+
+export function useAnalyticsServer() {
+  const postHogClient = getPostHogServerClient();
+
+  const capture = (
+    distinctId: string, // email or user id
+    event: string,
+    properties?: Record<string, unknown>,
+  ) => {
+    if (!postHogClient) {
+      return;
+    }
+
+    postHogClient.capture({ distinctId, event, properties });
+    console.log("tracked pageview", distinctId, event, properties);
+  };
+
+  const identify = (
+    distinctId: string, // email or user id
+    properties?: Record<string, unknown>,
+  ) => {
+    if (!postHogClient) {
+      return;
+    }
+
+    postHogClient.identify({ distinctId, properties });
+  };
+
+  return {
+    capture,
+    identify,
+  };
+}
+
+// DEPRECATED
+// export const analytics =
+//   process.env.JITSU_HOST && process.env.JITSU_WRITE_KEY
+//     ? jitsuAnalytics({
+//         host: process.env.JITSU_HOST,
+//         writeKey: process.env.JITSU_WRITE_KEY,
+//       })
+//     : emptyAnalytics;
+
+// const fetchUserWithCounts = async (userId: string) => {
+//   return await prisma.user.findUnique({
+//     where: { id: userId },
+//     select: {
+//       id: true,
+//       email: true,
+//       _count: {
+//         select: {
+//           documents: true,
+//           domains: true,
+//         },
+//       },
+//       documents: {
+//         select: {
+//           _count: {
+//             select: {
+//               links: true,
+//               views: true,
+//             },
+//           },
+//         },
+//       },
+//     },
+//   });
+// };
+
+// const getUserProperties = (user: any) => {
+//   let linkCount = user.documents.reduce(
+//     (acc: any, document: { _count: { links: number; views: number } }) =>
+//       acc + document._count.links,
+//     0,
+//   );
+
+//   let viewCount = user.documents.reduce(
+//     (acc: any, document: { _count: { links: number; views: number } }) =>
+//       acc + document._count.views,
+//     0,
+//   );
+
+//   const userData = {
+//     userId: user.id,
+//     email: user.email,
+//     documentCount: user._count.documents,
+//     domainCount: user._count.domains,
+//     linkCount: linkCount,
+//     viewCount: viewCount,
+//   };
+
+//   return userData;
+// };
+
+// export const identifyUser = async (userId: string) => {
+//   const user = await fetchUserWithCounts(userId);
+//   const userProperties = getUserProperties(user);
+//   analytics.identify(userId, userProperties);
+// };
+
+// export const trackAnalytics = (args: AnalyticsEvents) => analytics.track(args);
