a393cd687e0b644bd5f1f98591d2d6637e026a7d
Marc Seitz <marc@mfts.io>
2024-04-02 01:51:38 +1100

    feat: add folder tree view for viewing experience

--- components/datarooms/folders/utils.ts
+++ components/datarooms/folders/utils.ts
@@ -1,4 +1,5 @@
 import { DataroomFolderWithDocuments } from "@/lib/swr/use-dataroom";
+import { DataroomDocument, DataroomFolder } from "@prisma/client";
 
 // Helper function to build nested folder structure
 export const buildNestedFolderStructure = (
@@ -23,4 +24,55 @@ export const buildNestedFolderStructure = (
   });
 
   return rootFolders;
+};
+
+export const buildNestedFolderStructureWithDocs = (
+  folders: DataroomFolder[],
+  documents: DataroomDocumentWithVersion[],
+) => {
+  const folderMap = new Map();
+
+  // Initialize every folder with an additional childFolders property
+  folders.forEach((folder) => {
+    folderMap.set(folder.id, {
+      ...folder,
+      documents: documents.filter((doc) => doc.folderId === folder.id),
+      childFolders: [],
+    });
+  });
+
+  const rootFolders: DataroomFolderWithDocumentsNew[] = [];
+
+  folderMap.forEach((folder, id) => {
+    if (folder.parentId) {
+      const parent = folderMap.get(folder.parentId);
+      parent.childFolders.push(folder);
+    } else {
+      rootFolders.push(folder);
+    }
+  });
+
+  return rootFolders;
+};
+
+type DataroomDocumentWithVersion = {
+  dataroomDocumentId: string;
+  folderId: string | null;
+  id: string;
+  name: string;
+  versions: {
+    id: string;
+    versionNumber: number;
+    hasPages: boolean;
+  }[];
+};
+
+type DataroomFolderWithDocumentsNew = DataroomFolder & {
+  childFolders: DataroomFolderWithDocumentsNew[];
+  documents: {
+    dataroomDocumentId: string;
+    folderId: string | null;
+    id: string;
+    name: string;
+  }[];
 };
