1c04881106c1b5f3543542ddf3079689961225e7
Iuliia Shnai <yul.schnay@gmail.com>
2024-04-13 16:43:47 +1000

    updates for pricing

--- components/web/pricing-component.tsx
+++ components/web/pricing-component.tsx
@@ -14,24 +14,531 @@ const featureGroups: {
   }[];
 }[] = [
   {
-    name: "Users",
+    name: "Document Analytics and Tracking",
     features: [
       {
-        name: "Cost per user",
+        name: "Unlimited views recorded",
         id: "feature-user",
         tiers: {
-          free: "€0/month",
-          pro: "€10/month",
-          business: "€30/month",
-          enterprise: "Custom",
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
         },
       },
       {
-        name: "Cost 2 per user",
+        name: "Time spent on each page",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Views recorded for each user",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Real time feedback on each page",
         id: "feature-user",
         tiers: {
           free: false,
           pro: false,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Document versioning tracking",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Viever location tracking",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Exclude internal visits",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Unlimited view history",
+        id: "feature-user",
+        tiers: {
+          free: "up to 20",
+          pro: "up to 10",
+          business: true,
+          enterprise: true,
+        },
+      },
+    ],
+  },
+  {
+    name: "Link Settings",
+    features: [
+      {
+        name: "Capturing email to view",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Receive email notifications",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Allow/block document downloading",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Password protection",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Expiration date",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Email verification",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Allow/block specififed users",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: false,
+          business: true,
+          enterprise: true,
+        },
+      },
+    ],
+  },
+
+  {
+    name: "Data Rooms",
+    features: [
+      {
+        name: "Unlimited data rooms",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Unlimited documents",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Unlimited users",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "User groups",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: false,
+          business: false,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Folder",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "File size",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Custom branding",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Full whitelabelling",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: false,
+          business: false,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Self-hosted",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: false,
+          business: false,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Bulk upload",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: false,
+          business: false,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Expiration date",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Email verification",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Allow/block specififed users",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: false,
+          business: true,
+          enterprise: true,
+        },
+      },
+    ],
+  },
+  {
+    name: "Custom branding",
+    features: [
+      {
+        name: "Subdomain and slug",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Remove Papermark branding",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: "partial",
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Custom domain",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: "partial",
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Custom logo",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: "partial",
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Custom colors",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: "partial",
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Custom social media cards",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: false,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Custom questions",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: false,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Remove Papermark branding",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: "partial",
+          business: true,
+          enterprise: true,
+        },
+      },
+
+      {
+        name: "Custom dataroom banners",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: false,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Full whitelabelling",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: false,
+          business: false,
+          enterprise: true,
+        },
+      },
+      {
+        name: "SSO",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: false,
+          business: false,
+          enterprise: true,
+        },
+      },
+    ],
+  },
+  {
+    name: "Other",
+    features: [
+      {
+        name: "Notion documents integrations",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "AI-Document Assistant",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Forms",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Reactions",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: false,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Communication within document ",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+    ],
+  },
+  {
+    name: "Support",
+    features: [
+      {
+        name: "Migration from other document platform ",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Documentation",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "48h support",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Email support",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Support with self-hosting",
+        id: "feature-user",
+        tiers: {
+          free: false,
+          pro: false,
+          business: true,
+          enterprise: true,
+        },
+      },
+      {
+        name: "Custom features support",
+        id: "feature-user",
+        tiers: {
+          free: true,
+          pro: true,
           business: true,
           enterprise: true,
         },
