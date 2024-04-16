128166756e9b0b03aa20d03b382671a38285eb2e
Marc Seitz <marc@mfts.io>
2024-04-16 13:26:37 +1000

    feat: add question to links table

--- prisma/migrations/20240415000000_add_feedback/migration.sql
+++ prisma/migrations/20240415000000_add_feedback/migration.sql
@@ -1,8 +1,10 @@
+-- AlterTable
+ALTER TABLE "Link" ADD COLUMN     "enableQuestion" BOOLEAN DEFAULT false;
+
 -- CreateTable
 CREATE TABLE "Feedback" (
     "id" TEXT NOT NULL,
-    "documentId" TEXT NOT NULL,
-    "enabled" BOOLEAN NOT NULL DEFAULT false,
+    "linkId" TEXT NOT NULL,
     "data" JSONB NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -23,10 +25,10 @@ CREATE TABLE "FeedbackResponse" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Feedback_documentId_key" ON "Feedback"("documentId");
+CREATE UNIQUE INDEX "Feedback_linkId_key" ON "Feedback"("linkId");
 
 -- CreateIndex
-CREATE INDEX "Feedback_documentId_idx" ON "Feedback"("documentId");
+CREATE INDEX "Feedback_linkId_idx" ON "Feedback"("linkId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "FeedbackResponse_viewId_key" ON "FeedbackResponse"("viewId");
@@ -38,7 +40,7 @@ CREATE INDEX "FeedbackResponse_feedbackId_idx" ON "FeedbackResponse"("feedbackId
 CREATE INDEX "FeedbackResponse_viewId_idx" ON "FeedbackResponse"("viewId");
 
 -- AddForeignKey
-ALTER TABLE "Feedback" ADD CONSTRAINT "Feedback_documentId_fkey" FOREIGN KEY ("documentId") REFERENCES "Document"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Feedback" ADD CONSTRAINT "Feedback_linkId_fkey" FOREIGN KEY ("linkId") REFERENCES "Link"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "FeedbackResponse" ADD CONSTRAINT "FeedbackResponse_feedbackId_fkey" FOREIGN KEY ("feedbackId") REFERENCES "Feedback"("id") ON DELETE CASCADE ON UPDATE CASCADE;
