efa87ceb1814d615571e9c2fa2389a1854f191d9
Marc Seitz <marc@mfts.io>
2024-04-19 11:42:49 +0800

    feat: add delete folder

---
+++ pages/api/teams/[teamId]/datarooms/[id]/folders/manage/[folderId]/index.ts
@@ -0,0 +1,97 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth/next";
+import prisma from "@/lib/prisma";
+import { CustomUser } from "@/lib/types";
+import { errorhandler } from "@/lib/errorHandler";
+import slugify from "@sindresorhus/slugify";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "DELETE") {
+    // DELETE /api/teams/:teamId/datarooms/[:id]/folders/manage
+    const session = await getServerSession(req, res, authOptions);
+    if (!session) {
+      res.status(401).end("Unauthorized");
+      return;
+    }
+
+    const {
+      teamId,
+      id: dataroomId,
+      folderId,
+    } = req.query as {
+      teamId: string;
+      id: string;
+      folderId: string;
+    };
+
+    const userId = (session.user as CustomUser).id;
+
+    try {
+      const team = await prisma.team.findUnique({
+        where: {
+          id: teamId,
+          users: {
+            some: {
+              userId: userId,
+            },
+          },
+        },
+      });
+
+      if (!team) {
+        return res.status(401).end("Unauthorized");
+      }
+
+      const dataroom = await prisma.dataroom.findUnique({
+        where: {
+          id: dataroomId,
+          teamId: team.id,
+        },
+      });
+
+      if (!dataroom) {
+        return res.status(401).end("Dataroom not found");
+      }
+
+      const folder = await prisma.dataroomFolder.findUnique({
+        where: {
+          id: folderId,
+          dataroomId: dataroomId,
+        },
+        select: {
+          _count: {
+            select: {
+              documents: true,
+              childFolders: true,
+            },
+          },
+        },
+      });
+
+      if (folder?._count.documents! > 0 || folder?._count.childFolders! > 0) {
+        return res.status(401).json({
+          message: "Folder contains documents or folders. Move them first",
+        });
+      }
+
+      await prisma.dataroomFolder.delete({
+        where: {
+          id: folderId,
+          dataroomId: dataroomId,
+        },
+      });
+
+      return res.status(204).end(); // 204 No Content response for successful deletes
+    } catch (error) {
+      errorhandler(error, res);
+    }
+  } else {
+    // We only allow DELETE requests
+    res.setHeader("Allow", ["DELETE"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
