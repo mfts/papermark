99b4148c4b96f91780da9d1dbcfd7998d73a5a71
Marc Seitz <marc@mfts.io>
2024-04-29 12:51:44 +0400

    refactor: cleanup custom meta conditionals

--- components/view/custom-metatag.tsx
+++ components/view/custom-metatag.tsx
@@ -15,57 +15,62 @@ const CustomMetatag = ({
 }) => {
   return (
     <Head>
-      {
-        url && (
-          <>
-            <link rel="canonical" href={url} key="canonical" />
-            <meta property="og:url" content={url} key="og-url" />
-          </>
-        )
-      }
-      {title && <>
-        <title>{title}</title>
-        <meta property="og:title" content={title} key="og-title" />
-        <meta name="twitter:title" content={title} key="tw-title" />
-      </>}
-      {
-        !enableBranding && (
-          <>
-            <meta property="og:image" content={`/api/og?title=${title ?? ''}`} key="og-image" />
-            <meta name="twitter:image" content={`/api/og?title=${title ?? ''}`} key="tw-image" />
-          </>
-        )
-      }
-      {
-        enableBranding && (
-          <>
-            {description ? (
-              <meta name="description" content={description} key="description" />
-            ) : null}
-            {description ? (
+      {url && (
+        <>
+          <link rel="canonical" href={url} key="canonical" />
+          <meta property="og:url" content={url} key="og-url" />
+        </>
+      )}
+      {title && (
+        <>
+          <title>{title}</title>
+          <meta property="og:title" content={title} key="og-title" />
+          <meta name="twitter:title" content={title} key="tw-title" />
+        </>
+      )}
+      {!enableBranding && (
+        <>
+          <meta
+            property="og:image"
+            content={`/api/og?title=${title ?? ""}`}
+            key="og-image"
+          />
+          <meta
+            name="twitter:image"
+            content={`/api/og?title=${title ?? ""}`}
+            key="tw-image"
+          />
+        </>
+      )}
+      {enableBranding && (
+        <>
+          {description ? (
+            <>
+              <meta
+                name="description"
+                content={description}
+                key="description"
+              />
               <meta
                 property="og:description"
                 content={description}
                 key="og-description"
               />
-            ) : null}
-            {imageUrl ? (
-              <meta property="og:image" content={imageUrl} key="og-image" />
-            ) : null}
-            
-            {description ? (
               <meta
                 name="twitter:description"
                 content={description}
                 key="tw-description"
               />
-            ) : null}
-            {imageUrl ? (
+            </>
+          ) : null}
+          {imageUrl ? (
+            <>
+              <meta property="og:image" content={imageUrl} key="og-image" />
               <meta name="twitter:image" content={imageUrl} key="tw-image" />
-            ) : null}
-          </>
-        )
-      }
+            </>
+          ) : null}
+        </>
+      )}
     </Head>
   );
 };
