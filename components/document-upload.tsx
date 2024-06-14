6c214b5e500a89c11b12d718ec21be4b58b7d2bd
Marc Seitz <marc@mfts.io>
2024-06-14 12:53:21 +0200

    chore(temp): increase pages limit

--- components/document-upload.tsx
+++ components/document-upload.tsx
@@ -85,7 +85,7 @@ export default function DocumentUpload({
         .arrayBuffer()
         .then((buffer) => {
           getPagesCount(buffer).then((numPages) => {
-            if (numPages > 100) {
+            if (numPages > 250) {
               toast.error("File has too many pages (max. 100)");
             } else {
               setCurrentFile(file);
