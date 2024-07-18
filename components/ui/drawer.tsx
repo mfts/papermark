59bcd6eb7a3d5cb93e6c13829179bdbdb0124669
Marc Seitz <marc@mfts.io>
2024-07-18 14:12:08 +0200

    fix: notification drawer without snappoints

--- components/ui/drawer.tsx
+++ components/ui/drawer.tsx
@@ -42,7 +42,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
+        "fixed inset-x-0 bottom-0 z-50 flex h-auto flex-col rounded-t-[10px] border bg-background",
         className,
       )}
       onEscapeKeyDown={(e) => e.preventDefault()}
