2518aa6e3b238889656fd5ca61f18b11ee3664c0
Marc Seitz <marc@mfts.io>
2024-07-26 11:14:18 +0300

    fix: lint

--- lib/emails/send-onboarding.ts
+++ lib/emails/send-onboarding.ts
@@ -19,26 +19,36 @@ export const sendOnboardingEmail = async (
   params: CreateUserEmailProps,
   emailType: EmailType,
 ) => {
-  const { name, email } = params.user;
+  const { email } = params.user;
 
   let emailTemplate;
   let subject;
 
-  if (emailType === "onboarding1") {
-    emailTemplate = Onboarding1Email({ name });
-    subject = "Day 1 with Papermark - Turn your documents into links";
-  } else if (emailType === "onboarding2") {
-    emailTemplate = Onboarding2Email({ name });
-    subject = "Day 2 - Set link permissions";
-  } else if (emailType === "onboarding3") {
-    emailTemplate = Onboarding3Email({ name });
-    subject = "Day 3 - Track analytics on each page";
-  } else if (emailType === "onboarding4") {
-    emailTemplate = Onboarding4Email({ name });
-    subject = "Day 4 - Custom domain and branding";
-  } else if (emailType === "onboarding5") {
-    emailTemplate = Onboarding5Email({ name });
-    subject = "Day 5 - Virtual Data Rooms";
+  switch (emailType) {
+    case "onboarding1":
+      emailTemplate = Onboarding1Email();
+      subject = "Day 1 with Papermark - Turn your documents into links";
+      break;
+    case "onboarding2":
+      emailTemplate = Onboarding2Email();
+      subject = "Day 2 - Set link permissions";
+      break;
+    case "onboarding3":
+      emailTemplate = Onboarding3Email();
+      subject = "Day 3 - Track analytics on each page";
+      break;
+    case "onboarding4":
+      emailTemplate = Onboarding4Email();
+      subject = "Day 4 - Custom domain and branding";
+      break;
+    case "onboarding5":
+      emailTemplate = Onboarding5Email();
+      subject = "Day 5 - Virtual Data Rooms";
+      break;
+    default:
+      emailTemplate = Onboarding1Email();
+      subject = "Day 1 with Papermark - Turn your documents into links";
+      break;
   }
 
   try {
