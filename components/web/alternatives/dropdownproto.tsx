514f2443856c6cb8c4440b979ae6ccdba576b907
Marc Seitz <marc@mfts.io>
2024-07-31 12:03:42 +0200

    fix: missing key & formatting

--- components/web/alternatives/dropdownproto.tsx
+++ components/web/alternatives/dropdownproto.tsx
@@ -1,12 +1,15 @@
-import { CheckIcon} from "lucide-react";
 import { Dispatch, SetStateAction, useState } from "react";
-import * as React from "react"
+import * as React from "react";
+
+import { CheckIcon } from "lucide-react";
+
 import {
   Select,
   SelectContent,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
+
 import { cn } from "@/lib/utils";
 
 interface DropDownProps {
@@ -20,31 +23,30 @@ export default function DropDown({
   option,
   setOption,
 }: DropDownProps) {
-
   return (
     <Select>
-      <SelectTrigger className="inline-flex w-full justify-between items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-400 font-light shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue text-base">
-        <SelectValue placeholder={option}/>
+      <SelectTrigger className="focus:ring-blue inline-flex w-full items-center justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-light text-gray-400 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2">
+        <SelectValue placeholder={option} />
       </SelectTrigger>
       <SelectContent>
-        {options.map((optionItem) => (
+        {options.map((optionItem, index) => (
           <button
             onClick={() => {
-              setOption(optionItem)
+              setOption(optionItem);
             }}
             className={cn(
               option === optionItem ? "bg-gray-200" : "hover:bg-gray-100",
-              "px-4 py-2 text-sm w-full text-left flex items-center space-x-2 justify-between",
+              "flex w-full items-center justify-between space-x-2 px-4 py-2 text-left text-sm",
             )}
+            key={index}
           >
             <span>{optionItem}</span>
             {option === optionItem ? (
-              <CheckIcon className="w-4 h-4 text-bold" />
+              <CheckIcon className="text-bold h-4 w-4" />
             ) : null}
           </button>
         ))}
       </SelectContent>
     </Select>
-  )
+  );
 }
-
