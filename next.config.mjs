64bee33bbd403334e61a1fd73de71992e96f5d5f
Marc Seitz <marc@mfts.io>
2024-09-23 23:37:24 +0200

    fix: security headers

--- next.config.mjs
+++ next.config.mjs
@@ -37,7 +37,7 @@ const nextConfig = {
           },
           {
             key: "X-Frame-Options",
-            value: "DENY",
+            value: "SAMEORIGIN",
           },
         ],
       },
