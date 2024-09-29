f822a9aa395f8d0db7be7319face867f7ef81a8b
Marc Seitz <marc@mfts.io>
2024-09-30 00:05:29 +0200

    style: fix progress color

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -27,10 +27,7 @@ const Progress = React.forwardRef<
       {...props}
     >
       <ProgressPrimitive.Indicator
-        className={cn(
-          "h-full w-full flex-1 bg-primary transition-all",
-          value && value > 100 ? "bg-destructive dark:bg-red-600" : "",
-        )}
+        className="h-full w-full flex-1 bg-primary transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
       {text && !error ? (
