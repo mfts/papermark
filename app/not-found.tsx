fce03bab6062798dcf7c0e689f290f47ba01eb21
Aditya-PS-05 <adityapratapsjnhh7654@gmail.com>
2024-10-10 22:17:37 +0530

    not-found page added

---
+++ app/not-found.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
+const NotFound = () => {
+  return ( 
+    <div className="h-screen w-screen">
+      <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/_static/error.png"
+        height="300"
+        width="300"
+        alt="Error"
+        className="dark:hidden"
+      />
+      <Image
+        src="/_static/error-dark.png"
+        height="300"
+        width="300"
+        alt="Error"
+        className="hidden dark:block"
+      />
+      <h2 className="text-xl font-medium">
+        Something went wrong!
+      </h2>
+      <Button asChild>
+        <Link href="/">
+          Go back
+        </Link>
+      </Button>
+    </div>
+    </div>
+  );
+}
+ 
+export default NotFound;
