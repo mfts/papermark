f17f4130354d6f2fef2eb807e3ad819cc06d4e48
Meetpidev <meetkshah3112@gmail.com>
2024-10-11 13:29:19 +0530

    added close button in signup page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,9 +2,7 @@
 
 import Link from "next/link";
 import { useParams } from "next/navigation";
-
 import { useState } from "react";
-
 import { signInWithPasskey } from "@teamhanko/passkeys-next-auth-provider/client";
 import { Loader } from "lucide-react";
 import { signIn } from "next-auth/react";
@@ -17,7 +15,9 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
+
 export default function Login() {
+  
   const { next } = useParams as { next?: string };
 
   const [isLoginWithEmail, setIsLoginWithEmail] = useState<boolean>(false);
@@ -32,6 +32,16 @@ export default function Login() {
   return (
     <div className="flex h-screen w-full flex-wrap ">
       {/* Left part */}
+      
+      <button
+        className="absolute top-4 right-4 p-2 rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-white"
+        onClick={() => {
+          window.location.href = "https://www.papermark.io/";
+        }}
+      >
+        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+      </button>
+      
       <div className="flex w-full justify-center bg-white md:w-1/2 lg:w-2/5">
         <div
           className="absolute inset-x-0 top-10 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
