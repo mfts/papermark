ba9d9b066571f7ea5b28eea3ad1b0f18bd25368a
JeevaRamanathan <jeevaramanathan.m@infosys.com>
2024-10-23 02:55:07 +0530

    removed overflow hidden
    Signed-off-by: JeevaRamanathan <jeevaramanathan.m@infosys.com>

--- components/layouts/app.tsx
+++ components/layouts/app.tsx
@@ -6,7 +6,7 @@ import TrialBanner from "./trial-banner";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col overflow-hidden bg-gray-50 dark:bg-black lg:flex-row">
+    <div className="flex min-h-screen flex-col bg-gray-50 dark:bg-black lg:flex-row">
       <SidebarProvider>
         <AppSidebar />
         <div className="h-dvh flex-1">
@@ -14,9 +14,9 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
           <TrialBanner />
           <main className="h-dvh flex-1 lg:p-2">
             <div className="h-full overflow-y-auto rounded-xl bg-white ring-1 ring-gray-200 dark:border-none dark:bg-gray-900 dark:ring-gray-800">
-              <div className="sticky top-0 z-50 ml-2 pt-2 bg-white dark:bg-gray-900">
+              <div className="sticky top-0 z-50 ml-2 bg-white pt-2 dark:bg-gray-900">
                 <SidebarTrigger />
-                <Separator/>
+                <Separator />
               </div>
               {children}
             </div>
