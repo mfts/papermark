909ae1b9c1c8faee62471ce1ec61a94adefc2ec0
JeevaRamanathan <jeevaramanathan.m@infosys.com>
2024-10-22 22:40:05 +0530

    removed commented lines and improved sidebar responsiveness
    Signed-off-by: JeevaRamanathan <jeevaramanathan.m@infosys.com>

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,6 +10,7 @@ import { TeamContextType, initialState, useTeam } from "@/context/team-context";
 import Cookies from "js-cookie";
 import {
   CogIcon,
+  ContactIcon,
   FolderIcon as FolderLucideIcon,
   FolderOpenIcon,
   PaletteIcon,
@@ -29,10 +30,6 @@ import {
   SquareTerminal,
 } from "lucide-react";
 import { useSession } from "next-auth/react";
-
-import MenuIcon from "@/components/shared/icons/menu";
-import { NavMain } from "@/components/sidebar/nav-main";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import {
   Sidebar,
   SidebarContent,
@@ -54,299 +51,8 @@ import ProBanner from "./billing/pro-banner";
 import { UpgradePlanModal } from "./billing/upgrade-plan-modal";
 import ProfileMenu from "./profile-menu";
 import SiderbarFolders from "./sidebar-folders";
-import { AddTeamModal } from "./teams/add-team-modal";
 import SelectTeam from "./teams/select-team";
 import { Progress } from "./ui/progress";
-import { ScrollArea } from "./ui/scroll-area";
-
-// export default function Sidebar() {
-//   return (
-//     <>
-//       <nav>
-//         {/* sidebar for desktop */}
-//         <SidebarComponent className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex" />
-
-//         {/* move main screen to the right by width of the sidebar on desktop */}
-//         <div className="lg:pl-72"></div>
-//         {/* sidebar for mobile */}
-//         <div className="sticky top-0 z-40 mb-1 flex h-14 shrink-0 items-center border-b border-gray-50/90 bg-gray-50 px-6 dark:border-none dark:border-black/10 dark:bg-black/95 sm:px-12 lg:hidden">
-//           <Sheet>
-//             <SheetTrigger asChild>
-//               <button className="mt-1 p-0.5 text-muted-foreground lg:hidden">
-//                 <MenuIcon className="h-6 w-6" aria-hidden="true" />
-//               </button>
-//             </SheetTrigger>
-//             <SheetContent
-//               side="left"
-//               className="m-0 w-[280px] p-0 sm:w-[300px] lg:hidden"
-//             >
-//               <SidebarComponent className="flex" />
-//             </SheetContent>
-//           </Sheet>
-//           <div className="flex flex-1 items-center justify-end gap-x-4 self-stretch lg:gap-x-6">
-//             <ProfileMenu size="small" className="mr-3 mt-1.5" />
-//           </div>
-//         </div>
-//       </nav>
-//     </>
-//   );
-// }
-
-// export const SidebarComponent = ({ className }: { className?: string }) => {
-// const [showProBanner, setShowProBanner] = useState<boolean | null>(null);
-// const { data: session, status } = useSession();
-// const { plan: userPlan, trial: userTrial, loading } = usePlan();
-// const isTrial = !!userTrial;
-// const { limits } = useLimits();
-// const linksLimit = limits?.links;
-// const documentsLimit = limits?.documents;
-
-// const router = useRouter();
-// const { currentTeam, teams, isLoading }: TeamContextType =
-//   useTeam() || initialState;
-
-// useEffect(() => {
-//   if (Cookies.get("hideProBanner") !== "pro-banner") {
-//     setShowProBanner(true);
-//   } else {
-//     setShowProBanner(false);
-//   }
-// }, []);
-
-// const navigation = [
-//   // {
-//   //   name: "Overview",
-//   //   href: "/overview",
-//   //   icon: HomeIcon,
-//   //   current: router.pathname.includes("overview"),
-//   //   disabled: true,
-//   // },
-//   {
-//     name: "Documents",
-//     href: "/documents",
-//     icon:
-//       router.pathname.includes("documents") &&
-//       !router.pathname.includes("datarooms")
-//         ? FolderOpenIcon
-//         : FolderLucideIcon,
-//     current:
-//       router.pathname.includes("documents") &&
-//       !router.pathname.includes("tree") &&
-//       !router.pathname.includes("datarooms"),
-//     active:
-//       router.pathname.includes("documents") &&
-//       !router.pathname.includes("datarooms"),
-//     disabled: false,
-//   },
-//   {
-//     name: "Datarooms",
-//     href: "/datarooms",
-//     icon: ServerIcon,
-//     current: router.pathname.includes("datarooms"),
-//     active: false,
-//     disabled:
-//       userPlan === "business" ||
-//       userPlan === "datarooms" ||
-//       userTrial === "drtrial"
-//         ? false
-//         : true,
-//   },
-//   {
-//     name: "Branding",
-//     href: "/settings/branding",
-//     icon: PaletteIcon,
-//     current: router.pathname.includes("branding"),
-//     active: false,
-//     disabled: false,
-//   },
-//   {
-//     name: "Settings",
-//     href: "/settings/general",
-//     icon: CogIcon,
-//     current:
-//       router.pathname.includes("settings") &&
-//       !router.pathname.includes("branding") &&
-//       !router.pathname.includes("datarooms") &&
-//       !router.pathname.includes("documents"),
-//     active: false,
-//     disabled: false,
-//   },
-// ];
-
-//   return (
-//     <div>
-//       <aside
-//         className={cn(
-//           "h-dvh w-full flex-shrink-0 flex-col justify-between gap-y-6 bg-gray-50 px-4 pt-4 dark:bg-black lg:w-72 lg:px-6 lg:pt-6",
-//           className,
-//         )}
-//       >
-//         {/* Sidebar component, swap this element with another sidebar if you like */}
-
-//         <div className="flex h-16 shrink-0 items-center space-x-3">
-//           <p className="flex items-center text-2xl font-bold tracking-tighter text-black dark:text-white">
-//             <Link href="/documents">Papermark{" "}</Link>
-// {userPlan && userPlan != "free" ? (
-//   <span className="ml-4 rounded-full bg-background px-2.5 py-1 text-xs tracking-normal text-foreground ring-1 ring-gray-800">
-//     {userPlan.charAt(0).toUpperCase() + userPlan.slice(1)}
-//   </span>
-// ) : null}
-// {isTrial ? (
-//   <span className="ml-4 rounded-sm bg-foreground px-2 py-0.5 text-xs tracking-normal text-background ring-1 ring-gray-800">
-//     Trial
-//   </span>
-// ) : null}
-//           </p>
-//         </div>
-
-// <SelectTeam
-//   currentTeam={currentTeam}
-//   teams={teams}
-//   isLoading={isLoading}
-//   setCurrentTeam={() => {}}
-// />
-
-//         {/* <div className="flex items-center gap-x-1">
-//           <AddDocumentModal>
-//             <Button
-//               className="flex-1 text-left group flex gap-x-3 items-center justify-start px-3"
-//               title="Add New Document"
-//             >
-//               <PlusIcon className="h-5 w-5 shrink-0" aria-hidden="true" />
-//               <span>Add New Document</span>
-//             </Button>
-//           </AddDocumentModal>
-//           <AddFolderModal>
-//             <Button
-//               size="icon"
-//               variant="outline"
-//               className="bg-gray-50 dark:bg-black border-gray-500 hover:bg-gray-200 hover:dark:bg-muted"
-//             >
-//               <FolderPlusIcon className="w-5 h-5 shrink-0" aria-hidden="true" />
-//             </Button>
-//           </AddFolderModal>
-//         </div> */}
-
-//         <ScrollArea className="flex-grow" showScrollbar>
-//           <section className="flex flex-1 flex-col gap-y-6">
-//             <div className="space-y-2">
-//               {navigation.map((item) => {
-//                 if (item.name === "Documents") {
-//                   return (
-//                     <div key={item.name}>
-//                       <button
-//                         onClick={() => router.push(item.href)}
-//                         disabled={item.disabled}
-//                         className={cn(
-//                           item.current
-//                             ? "bg-gray-200 font-semibold text-foreground dark:bg-secondary"
-//                             : "duration-200 hover:bg-gray-200 hover:dark:bg-muted",
-//                           "group flex w-full items-center gap-x-2 rounded-md px-3 py-2 text-sm leading-6 disabled:cursor-default disabled:text-muted-foreground disabled:hover:bg-transparent",
-//                         )}
-//                       >
-//                         <item.icon
-//                           className="h-5 w-5 shrink-0"
-//                           aria-hidden="true"
-//                         />
-//                         {item.name}
-//                       </button>
-//                       {item.active ? <SiderbarFolders /> : null}
-//                     </div>
-//                   );
-//                 }
-// if (
-//   userPlan !== "business" &&
-//   userPlan !== "datarooms" &&
-//   userTrial !== "drtrial" &&
-//   item.name === "Datarooms"
-// ) {
-//   return (
-// <UpgradePlanModal
-//   key={item.name}
-//   clickedPlan={"Business"}
-//   trigger={"datarooms"}
-// >
-// <div className="group flex w-full items-center gap-x-2 rounded-md px-3 py-2 text-sm leading-6 text-muted-foreground hover:bg-transparent">
-//   <item.icon
-//     className="h-5 w-5 shrink-0"
-//     aria-hidden="true"
-//   />
-//   {item.name}
-// </div>
-//                     </UpgradePlanModal>
-//                   );
-//                 }
-// return (
-//   <button
-//     key={item.name}
-//     onClick={() => router.push(item.href)}
-//     disabled={item.disabled}
-//     className={cn(
-//       item.current
-//         ? "bg-gray-200 font-semibold text-foreground dark:bg-secondary"
-//         : "duration-200 hover:bg-gray-200 hover:dark:bg-muted",
-//       "group flex w-full items-center gap-x-2 rounded-md px-3 py-2 text-sm leading-6 disabled:cursor-default disabled:text-muted-foreground disabled:hover:bg-transparent",
-//     )}
-//   >
-//     <item.icon
-//       className="h-5 w-5 shrink-0"
-//       aria-hidden="true"
-//     />
-//     {item.name}
-//   </button>
-// );
-//               })}
-//             </div>
-//           </section>
-//         </ScrollArea>
-//     <div className="mb-4">
-//       {/* if user is on trial show banner,
-//        * if user is pro show nothing,
-//        * if user is free and showProBanner is true show pro banner
-//        */}
-//       {userPlan === "trial" && session ? (
-//         <Banner session={session} />
-//       ) : null}
-//       {(userPlan === "pro" ||
-//         userPlan === "business" ||
-//         userPlan === "datarooms") &&
-//         null}
-//       {userPlan === "free" && showProBanner ? (
-//         <ProBanner setShowProBanner={setShowProBanner} />
-//       ) : null}
-
-//       <div className="mb-2">
-//         {linksLimit ? (
-//           <UsageProgress
-//             title="Links"
-//             unit="links"
-//             usage={limits?.usage?.links}
-//             usageLimit={linksLimit}
-//           />
-//         ) : null}
-//         {documentsLimit ? (
-//           <UsageProgress
-//             title="Documents"
-//             unit="documents"
-//             usage={limits?.usage?.documents}
-//             usageLimit={documentsLimit}
-//           />
-//         ) : null}
-//         {linksLimit || documentsLimit ? (
-//           <p className="mt-2 px-2 text-xs text-muted-foreground">
-//             Change plan to increase usage limits
-//           </p>
-//         ) : null}
-//       </div>
-
-//       <div className="hidden w-full lg:block">
-//         <ProfileMenu size="large" />
-//       </div>
-//     </div>
-//   </aside>
-// </div>
-//   );
-// };
 
 function UsageProgress(data: {
   title: string;
@@ -442,6 +148,14 @@ export default function AppSidebar({
           : true,
     },
     {
+      name: "Visitors",
+      href: "/visitors",
+      icon: ContactIcon,
+      current: router.pathname.includes("visitors"),
+      active: false,
+      disabled: userPlan === "free" && !isTrial ? true : false,
+    },
+    {
       name: "Branding",
       href: "/settings/branding",
       icon: PaletteIcon,
@@ -471,7 +185,7 @@ export default function AppSidebar({
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <a href="#">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                   P {/*should be replaced by actual logo*/}
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
@@ -495,18 +209,17 @@ export default function AppSidebar({
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
+        <div className="mt-4">
+          <SelectTeam
+            currentTeam={currentTeam}
+            teams={teams}
+            isLoading={isLoading}
+            setCurrentTeam={() => {}}
+          />
+        </div>
       </SidebarHeader>
-      <SidebarContent>
-        <SelectTeam
-          currentTeam={currentTeam}
-          teams={teams}
-          isLoading={isLoading}
-          setCurrentTeam={() => {}}
-        />
-        {/* <NavMain items={navigation} /> */}
-        {/* <NavProjects projects={data.projects} /> */}
-        {/* <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
 
+      <SidebarContent>
         <SidebarGroup>
           <SidebarMenu>
             {navigation.map((item) => {
@@ -517,6 +230,7 @@ export default function AppSidebar({
                       asChild
                       tooltip={item.name}
                       isActive={item.current}
+                      className="cursor-pointer"
                       onClick={() => router.push(item.href)}
                     >
                       <a>
@@ -531,14 +245,14 @@ export default function AppSidebar({
               if (
                 userPlan !== "business" &&
                 userPlan !== "datarooms" &&
-                userTrial !== "drtrial" &&
+                !isTrial &&
                 item.name === "Datarooms"
               ) {
                 return (
                   <UpgradePlanModal
                     key={item.name}
                     clickedPlan={"Business"}
-                    trigger={"datarooms"}
+                    trigger={"sidebar_datarooms"}
                   >
                     <SidebarMenuItem key={item.name}>
                       <SidebarMenuButton
@@ -546,7 +260,6 @@ export default function AppSidebar({
                         asChild
                         tooltip={item.name}
                         isActive={item.current}
-                        onClick={() => router.push(item.href)}
                       >
                         <a>
                           {/* <div className="group flex w-full items-center gap-x-2 rounded-md py-2 ml-2 text-sm leading-6 text-muted-foreground hover:bg-transparent"> */}
@@ -562,6 +275,30 @@ export default function AppSidebar({
                   </UpgradePlanModal>
                 );
               }
+              if (userPlan == "free" && !isTrial && item.name === "Visitors") {
+                return (
+                  <UpgradePlanModal
+                    key={item.name}
+                    clickedPlan={"Pro"}
+                    trigger={"sidebar_visitors"}
+                  >
+                    <SidebarMenuButton
+                      className="text-sm leading-6 text-muted-foreground hover:bg-inherit hover:text-muted-foreground"
+                      asChild
+                      tooltip={item.name}
+                      isActive={item.current}
+                    >
+                      <a>
+                        <item.icon
+                          className="h-5 w-5 shrink-0"
+                          aria-hidden="true"
+                        />
+                        {item.name}
+                      </a>
+                    </SidebarMenuButton>
+                  </UpgradePlanModal>
+                );
+              }
 
               return (
                 <SidebarMenuItem key={item.name}>
@@ -570,6 +307,7 @@ export default function AppSidebar({
                     tooltip={item.name}
                     isActive={item.current}
                     onClick={() => router.push(item.href)}
+                    className="cursor-pointer"
                   >
                     <a>
                       <item.icon
--- components/teams/select-team.tsx
+++ components/teams/select-team.tsx
@@ -16,6 +16,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
 
 import { Team } from "@/lib/types";
 import { cn } from "@/lib/utils";
@@ -33,92 +34,79 @@ const SelectTeam = ({ teams, currentTeam, isLoading }: TeamContextType) => {
   return (
     <>
       {isLoading ? (
-        <div className="flex items-center gap-2 text-sm">
-          <Loader className="h-5 w-5 animate-spin" /> Loading teams...
-        </div>
+        <SidebarMenuButton
+          size="sm"
+          className="flex cursor-pointer items-center justify-between rounded-md border p-5 px-[10px] opacity-90 hover:bg-muted"
+        >
+          <div className="flex aspect-square items-center space-x-4">
+            <Skeleton className="h-6 w-6 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[150px]" />
+            </div>
+          </div>
+        </SidebarMenuButton>
       ) : (
         <SidebarMenu>
-      <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-          {/* <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-            <div className="flex cursor-pointer items-center justify-between rounded-md border px-[10px] py-2 opacity-90 duration-200 hover:bg-muted">
-              <div className="flex items-center space-x-2">
-                <Avatar className="h-[25px] w-[25px] text-[10px]">
-                  <AvatarFallback>
-                    {currentTeam?.name?.slice(0, 2).toUpperCase()}
-                  </AvatarFallback>
-                </Avatar>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton
+                  size="sm"
+                  className="flex cursor-pointer items-center justify-between rounded-md border p-6 px-[10px] opacity-90 hover:bg-muted"
+                >
+                  <div className="flex aspect-square size-4 items-center justify-center rounded-lg">
+                    <Avatar className="h-[25px] w-[25px] text-[10px]">
+                      <AvatarFallback>
+                        {currentTeam?.name?.slice(0, 2).toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                  </div>
 
-                <p className="text-sm">{currentTeam?.name}</p>
-              </div>
-              <ChevronUpDownIcon className="h-4 w-4" />
-            </div>
-            </SidebarMenuButton> */}
+                  <div className="ml-2 grid flex-1 text-left text-sm leading-tight">
+                    <p className="text-sm">{currentTeam?.name}</p>
+                  </div>
+                  <ChevronUpDownIcon className="h-4 w-4" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent sideOffset={4} className="z-50 w-64">
+                {teams.map((team) => (
+                  <div
+                    key={team.id}
+                    onClick={() => switchTeam(team)}
+                    className={cn(
+                      `flex w-full cursor-pointer items-center justify-between truncate px-3 py-2 text-sm font-normal transition-all duration-75 hover:bg-gray-200 hover:dark:bg-gray-900`,
+                      team.id === currentTeam?.id && "font-medium",
+                    )}
+                  >
+                    <div className="flex items-center space-x-2">
+                      <Avatar className="h-7 w-7 text-xs">
+                        <AvatarFallback>
+                          {team.name?.slice(0, 2).toUpperCase()}
+                        </AvatarFallback>
+                      </Avatar>
 
-<SidebarMenuButton
-              size="lg"
-              className=" p-5 data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-              <Avatar className="h-[25px] w-[25px] text-[10px]">
-              <AvatarFallback>
-                    {currentTeam?.name?.slice(0, 2).toUpperCase()}
-                  </AvatarFallback>
-                  </Avatar>
-              </div>
-              
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">
-                 Sample Value
-                </span>
-                <span className="truncate text-xs">Dummy</span>
-              </div>
-              <ChevronUpDownIcon className="ml-auto" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent sideOffset={4} className="w-64">
-            {teams.map((team) => (
-              <div
-                key={team.id}
-                onClick={() => switchTeam(team)}
-                className={cn(
-                  `flex w-full cursor-pointer items-center justify-between truncate px-3 py-2 text-sm font-normal transition-all duration-75 hover:bg-gray-200 hover:dark:bg-gray-800`,
-                  team.id === currentTeam?.id && "font-medium",
-                )}
-              >
-                <div className="flex items-center space-x-2">
-                  <Avatar className="h-7 w-7 text-xs">
-                    <AvatarFallback>
-                      {team.name?.slice(0, 2).toUpperCase()}
-                    </AvatarFallback>
-                  </Avatar>
+                      <p>{team.name}</p>
+                    </div>
 
-                  <p>{team.name}</p>
-                </div>
+                    {team.id === currentTeam?.id && (
+                      <Check
+                        className="h-4 w-4 text-black dark:text-white"
+                        aria-hidden="true"
+                      />
+                    )}
+                  </div>
+                ))}
 
-                {team.id === currentTeam?.id && (
-                  <Check
-                    className="h-4 w-4 text-black dark:text-white"
-                    aria-hidden="true"
-                  />
-                )}
-              </div>
-            ))}
-
-            <Link
-              href="/settings/people"
-              className="mx-auto mb-1 mt-3 flex w-[92%] items-center rounded-sm border px-[10px] py-2 text-sm duration-100 hover:cursor-pointer hover:bg-gray-200 hover:dark:bg-gray-800"
-            >
-              <PlusIcon className="mr-2 h-4 w-4" />
-              Invite Members
-            </Link>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        </SidebarMenuItem>
+                <Link
+                  href="/settings/people"
+                  className="mx-auto mb-1 mt-3 flex w-[92%] items-center rounded-sm border px-[10px] py-2 text-sm duration-100 hover:cursor-pointer hover:bg-gray-200 hover:dark:bg-gray-800"
+                >
+                  <PlusIcon className="mr-2 h-4 w-4" />
+                  Invite Members
+                </Link>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
         </SidebarMenu>
       )}
     </>
