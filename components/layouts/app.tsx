e941f64a5395b45995c4204ec1ded784fa739f51
JeevaRamanathan <jeevaramanathan.m@infosys.com>
2024-10-23 02:50:13 +0530

    changed sidebar trigger position
    Signed-off-by: JeevaRamanathan <jeevaramanathan.m@infosys.com>

--- components/layouts/app.tsx
+++ components/layouts/app.tsx
@@ -1,5 +1,7 @@
 import AppSidebar from "@/components/Sidebar";
+import { Separator } from "@/components/ui/separator";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
 import TrialBanner from "./trial-banner";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
@@ -11,8 +13,11 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
           {/* Trial banner shown only on trial */}
           <TrialBanner />
           <main className="h-dvh flex-1 lg:p-2">
-            <SidebarTrigger />
             <div className="h-full overflow-y-auto rounded-xl bg-white ring-1 ring-gray-200 dark:border-none dark:bg-gray-900 dark:ring-gray-800">
+              <div className="sticky top-0 z-50 ml-2 pt-2 bg-white dark:bg-gray-900">
+                <SidebarTrigger />
+                <Separator/>
+              </div>
               {children}
             </div>
           </main>
