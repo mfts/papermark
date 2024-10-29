e4b7ef49eb0396d379f3bc12dbfde5eb168b7954
Ashish Viradiya <116018976+AshishViradiya153@users.noreply.github.com>
2024-10-29 09:36:56 +0530

    fix 2 scrollbar on smallscreen (#1309)

--- components/layouts/app.tsx
+++ components/layouts/app.tsx
@@ -8,7 +8,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       <div className="h-dvh flex-1">
         {/* Trial banner shown only on trial */}
         <TrialBanner />
-        <main className="h-dvh flex-1 lg:p-2">
+        <main className="h-[calc(100dvh-60px)] flex-1 lg:h-dvh lg:p-2">
           <div className="h-full overflow-y-auto rounded-xl bg-white ring-1 ring-gray-200 dark:border-none dark:bg-gray-900 dark:ring-gray-800">
             {children}
           </div>
