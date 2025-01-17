2d928ac817db067716524a259e436f81e1226460
Marc Seitz <marc@mfts.io>
2025-01-17 13:33:18 +0100

    feat: add back to all datarooms button

--- components/datarooms/dataroom-header.tsx
+++ components/datarooms/dataroom-header.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 import { useState } from "react";
 
-import LinkSheet from "../links/link-sheet";
-import { Button } from "../ui/button";
+import LinkSheet from "@/components/links/link-sheet";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
 
 export const DataroomHeader = ({
   title,
@@ -22,36 +31,37 @@ export const DataroomHeader = ({
   }
 
   return (
-    <section className="mb-4 flex items-center justify-between">
-      <div className="flex min-h-10 items-center space-y-1">
-        <h1 className="text-xl font-semibold tracking-tight text-foreground sm:text-2xl">
-          {title}
-        </h1>
-        {/* <p className="text-xs sm:text-sm text-muted-foreground font-mono">
-          {description}
-        </p> */}
+    <section className="mb-4">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/datarooms">All Datarooms</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <span className="font-medium text-foreground">{title}</span>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div className="mt-2 flex items-center justify-between">
+        <div className="flex min-h-10 items-center space-y-1">
+          <h1 className="text-xl font-semibold tracking-tight text-foreground sm:text-2xl">
+            {title}
+          </h1>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <Button onClick={() => setIsLinkSheetOpen(true)} key={1}>
+            Share
+          </Button>
+        </div>
+        <LinkSheet
+          linkType={"DATAROOM_LINK"}
+          isOpen={isLinkSheetOpen}
+          setIsOpen={setIsLinkSheetOpen}
+        />
       </div>
-      <div className="flex items-center gap-x-1">
-        <Button onClick={() => setIsLinkSheetOpen(true)} key={1}>
-          Share
-        </Button>
-        {/* {actionRows.map((row, i) => (
-          <ul
-            key={i.toString()}
-            className="flex flex-wrap items-center justify-end gap-2 md:flex-nowrap md:gap-4"
-          >
-            {row.map((action, i) => (
-              <li key={i}>{action}</li>
-            ))}
-          </ul>
-        ))} */}
-      </div>
-      <LinkSheet
-        linkType={"DATAROOM_LINK"}
-        isOpen={isLinkSheetOpen}
-        setIsOpen={setIsLinkSheetOpen}
-        // existingLinks={links}
-      />
     </section>
   );
 };
