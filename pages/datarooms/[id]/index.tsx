7e4c683ede5a4e49a1b73de210443025732cc7be
Marc Seitz <marc@mfts.io>
2025-01-18 13:08:29 +0100

    fix: applayout

--- pages/datarooms/[id]/index.tsx
+++ pages/datarooms/[id]/index.tsx
@@ -22,12 +22,7 @@ export default function DataroomPage() {
   }
 
   return (
-    <AppLayout
-      breadcrumbs={[
-        { title: "All Datarooms", href: "/datarooms" },
-        { title: dataroom.name, href: `/datarooms/${dataroom.id}` },
-      ]}
-    >
+    <AppLayout>
       <div className="relative mx-2 mb-10 mt-4 space-y-8 overflow-hidden px-1 sm:mx-3 md:mx-5 md:mt-5 lg:mx-7 lg:mt-8 xl:mx-10">
         <header>
           <DataroomHeader
