6c891a50ea8403c1bbd61ba398fe15d35450cfee
Marc Seitz <marc@mfts.io>
2025-02-12 17:01:45 +0100

    fix: sidebar disabled button

--- components/sidebar/nav-main.tsx
+++ components/sidebar/nav-main.tsx
@@ -53,16 +53,21 @@ export function NavMain({ items }: { items: NavItem[] }) {
                   "rounded-md bg-gray-200 font-semibold dark:bg-secondary",
               )}
             >
-              <SidebarMenuButton asChild={!item.disabled} tooltip={item.title}>
+              <SidebarMenuButton asChild tooltip={item.title}>
                 {item.disabled ? (
                   <UpgradePlanModal
                     key={item.title}
                     clickedPlan={item.plan as PlanEnum}
                     trigger={item.trigger}
                   >
-                    <div className="flex w-full flex-row items-center gap-2">
-                      <item.icon className="h-4 w-4" />
-                      <span id={item.plan}>{item.title}</span>
+                    <div className="peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0">
+                      <item.icon />
+                      <span
+                        className="group-data-[collapsible=icon]:hidden"
+                        id={item.plan}
+                      >
+                        {item.title}
+                      </span>
                     </div>
                   </UpgradePlanModal>
                 ) : (
