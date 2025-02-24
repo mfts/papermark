2fb0dbc3551ff5c817815612fa434eb585526342
Ashish Viradiya <ashishviradiya153@gmail.com>
2025-02-24 22:40:13 +0530

    ui improvement

--- components/sidebar/nav-main.tsx
+++ components/sidebar/nav-main.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 
-import { ChevronRight, type LucideIcon } from "lucide-react";
+import { ChevronRight, CrownIcon, type LucideIcon } from "lucide-react";
 
 import {
   Collapsible,
@@ -60,13 +60,14 @@ export function NavMain({ items }: { items: NavItem[] }) {
                     clickedPlan={item.plan as PlanEnum}
                     trigger={item.trigger}
                   >
-                    <div className="peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0">
+                    <div className="peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm text-muted-foreground outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0">
                       <item.icon />
                       <span
-                        className="group-data-[collapsible=icon]:hidden"
+                        className="flex flex-row items-center gap-2 group-data-[collapsible=icon]:hidden"
                         id={item.plan}
                       >
                         {item.title}
+                        <CrownIcon className="!size-4" />
                       </span>
                     </div>
                   </UpgradePlanModal>
