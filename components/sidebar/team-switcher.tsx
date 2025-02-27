c9d64e3145038405f9b248c6d442fc26a4ae62e1
Ashish Viradiya <ashishviradiya153@gmail.com>
2025-02-28 01:40:56 +0530

    Added 'Add User' button to the sidebar

--- components/sidebar/team-switcher.tsx
+++ components/sidebar/team-switcher.tsx
@@ -2,7 +2,15 @@
 
 import * as React from "react";
 
-import { ChevronsUpDown, GalleryVerticalEndIcon, Plus } from "lucide-react";
+import { useLimits } from "@/ee/limits/swr-handler";
+import { PlanEnum } from "@/ee/stripe/constants";
+import {
+  ChevronsUpDown,
+  GalleryVerticalEndIcon,
+  Loader,
+  Plus,
+  UserRoundPlusIcon,
+} from "lucide-react";
 
 import {
   DropdownMenu,
@@ -20,10 +28,17 @@ import {
   useSidebar,
 } from "@/components/ui/sidebar";
 
+import { useInvitations } from "@/lib/swr/use-invitations";
+import { useGetTeam } from "@/lib/swr/use-team";
 import { Team } from "@/lib/types";
 import { cn } from "@/lib/utils";
 
+import { AddSeatModal } from "../billing/add-seat-modal";
+import { UpgradePlanModal } from "../billing/upgrade-plan-modal";
+import { AddTeamMembers } from "../teams/add-team-member-modal";
 import { Avatar, AvatarFallback } from "../ui/avatar";
+import { Button } from "../ui/button";
+import { ButtonTooltip } from "../ui/tooltip";
 
 export function TeamSwitcher({
   currentTeam: activeTeam,
@@ -34,7 +49,16 @@ export function TeamSwitcher({
   teams: Team[];
   setCurrentTeam: (team: Team) => void;
 }) {
+  const [isTeamMemberInviteModalOpen, setTeamMemberInviteModalOpen] =
+    React.useState<boolean>(false);
+  const [isAddSeatModalOpen, setAddSeatModalOpen] =
+    React.useState<boolean>(false);
+  const { team, loading } = useGetTeam()!;
   const { isMobile } = useSidebar();
+  const { limits } = useLimits();
+  const { invitations } = useInvitations();
+  const numUsers = (team && team.users.length) ?? 1;
+  const numInvitations = (invitations && invitations.length) ?? 0;
 
   const switchTeam = (team: Team) => {
     localStorage.setItem("currentTeamId", team.id);
@@ -44,13 +68,13 @@ export function TeamSwitcher({
   if (!activeTeam) return null;
 
   return (
-    <SidebarMenu>
+    <SidebarMenu className="flex flex-row gap-1 group-data-[collapsible=icon]:flex-col group-data-[collapsible=icon]:gap-1.5">
       <SidebarMenuItem>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton
               size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground border"
+              className="border data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="size-8 rounded">
                 <AvatarFallback className="rounded">
@@ -102,6 +126,61 @@ export function TeamSwitcher({
             </DropdownMenuItem> */}
           </DropdownMenuContent>
         </DropdownMenu>
+      </SidebarMenuItem>
+      <SidebarMenuItem>
+        {activeTeam.plan === "free" ? (
+          <UpgradePlanModal
+            clickedPlan={PlanEnum.Pro}
+            trigger={"invite_team_members"}
+          >
+            <SidebarMenuButton
+              size="lg"
+              className="border data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <Button
+                type="button"
+                variant="ghost"
+                className="h-8 w-8 bg-gray-300 p-1 dark:bg-muted"
+              >
+                <UserRoundPlusIcon className="!h-4 !w-4" />
+              </Button>
+            </SidebarMenuButton>
+          </UpgradePlanModal>
+        ) : limits === null ||
+          (limits && limits.users > numUsers + numInvitations) ? (
+          <AddTeamMembers
+            open={isTeamMemberInviteModalOpen}
+            setOpen={setTeamMemberInviteModalOpen}
+          >
+            <SidebarMenuButton
+              size="lg"
+              className="border data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <Button
+                type="button"
+                variant="ghost"
+                className="h-8 w-8 bg-gray-300 p-1 dark:bg-muted"
+              >
+                <UserRoundPlusIcon className="!h-4 !w-4" />
+              </Button>
+            </SidebarMenuButton>
+          </AddTeamMembers>
+        ) : (
+          <AddSeatModal open={isAddSeatModalOpen} setOpen={setAddSeatModalOpen}>
+            <SidebarMenuButton
+              size="lg"
+              className="border data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <Button
+                type="button"
+                variant="ghost"
+                className="h-8 w-8 bg-gray-300 p-1 dark:bg-muted"
+              >
+                <UserRoundPlusIcon className="!h-4 !w-4" />
+              </Button>
+            </SidebarMenuButton>
+          </AddSeatModal>
+        )}
       </SidebarMenuItem>
     </SidebarMenu>
   );
