81211ee7ad56e38b4afccaddad8e2cb135c3d89f
Marc Seitz <marc@mfts.io>
2025-03-04 12:07:17 +0100

    refactor: change page name

--- components/datarooms/groups/group-navigation.tsx
+++ components/datarooms/groups/group-navigation.tsx
@@ -1,7 +1,13 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-import { CogIcon, FileSlidersIcon, LinkIcon, UsersIcon } from "lucide-react";
+import {
+  ChartColumnIcon,
+  CogIcon,
+  FileSlidersIcon,
+  LinkIcon,
+  UsersIcon,
+} from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
@@ -54,18 +60,6 @@ export const GroupNavigation = ({
         Permissions
       </Link>
       <Link
-        href={`/datarooms/${dataroomId}/groups/${viewerGroupId}/group-analytic`}
-        className={cn(
-          "flex items-center gap-x-2 rounded-md p-2 text-primary hover:bg-muted",
-          {
-            "bg-muted font-medium": router.pathname.includes("group-analytic"),
-          },
-        )}
-      >
-        <FileSlidersIcon className="h-4 w-4" />
-        Group analytics
-            </Link>
-          <Link
         href={`/datarooms/${dataroomId}/groups/${viewerGroupId}/links`}
         className={cn(
           "flex items-center gap-x-2 rounded-md p-2 text-primary hover:bg-muted",
@@ -76,7 +70,18 @@ export const GroupNavigation = ({
       >
         <LinkIcon className="h-4 w-4" />
         Links
-
+      </Link>
+      <Link
+        href={`/datarooms/${dataroomId}/groups/${viewerGroupId}/group-analytics`}
+        className={cn(
+          "flex items-center gap-x-2 rounded-md p-2 text-primary hover:bg-muted",
+          {
+            "bg-muted font-medium": router.pathname.includes("group-analytic"),
+          },
+        )}
+      >
+        <ChartColumnIcon className="h-4 w-4" />
+        Group analytics
       </Link>
     </nav>
   );
