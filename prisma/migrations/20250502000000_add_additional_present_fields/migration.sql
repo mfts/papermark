9b957264a57bccc7f3d8d699180d2a8066bd1860
Marc Seitz <marc@mfts.io>
2025-05-02 14:15:14 +0200

    refactor: cleanup migration

--- prisma/migrations/20250502000000_add_additional_present_fields/migration.sql
+++ prisma/migrations/20250502000000_add_additional_present_fields/migration.sql
@@ -3,6 +3,5 @@ ALTER TABLE "LinkPreset" ADD COLUMN     "agreementId" TEXT,
 ADD COLUMN     "customFields" JSONB,
 ADD COLUMN     "enableAgreement" BOOLEAN DEFAULT false,
 ADD COLUMN     "enableCustomFields" BOOLEAN DEFAULT false,
-DROP COLUMN "expiresIn",
-ADD COLUMN     "expiresIn" INTEGER;
+ADD COLUMN     "enableScreenshotProtection" BOOLEAN DEFAULT false;
 
