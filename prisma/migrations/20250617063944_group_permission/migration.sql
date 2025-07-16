750eac765ea257d010132f5e06fd83e262e24908
Marc Seitz <marc@mfts.io>
2025-07-16 12:49:49 +0200

    feat: simplify migration file

--- prisma/migrations/20250617063944_group_permission/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateEnum
-CREATE TYPE "DefaultGroupPermissionStrategy" AS ENUM ('ask_every_time', 'inherit_from_parent', 'use_default_permissions', 'no_permissions');
-
--- AlterTable
-ALTER TABLE "Dataroom" ADD COLUMN     "defaultGroupPermission" "DefaultGroupPermissionStrategy" NOT NULL DEFAULT 'ask_every_time';
-
--- AlterTable
-ALTER TABLE "ViewerGroup" ADD COLUMN     "defaultCanDownload" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "defaultCanView" BOOLEAN NOT NULL DEFAULT false;
