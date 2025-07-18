a22550494bbb5e077e048ae7a26e3a3f540ed9e3
Marc Seitz <marc@mfts.io>
2025-07-18 12:04:35 +0200

    fix(emails): remove visited css

--- components/emails/email-updated.tsx
+++ components/emails/email-updated.tsx
@@ -53,7 +53,7 @@ export function EmailUpdated({
                 © {new Date().getFullYear()}{" "}
                 <a
                   href="https://www.papermark.com"
-                  className="text-gray-400 no-underline visited:text-gray-400 hover:text-gray-400"
+                  className="text-gray-400 no-underline hover:text-gray-400"
                   target="_blank"
                 >
                   papermark.com
