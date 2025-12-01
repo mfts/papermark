30278876d3324bc72da860429446204ae0e6b515
Marc Seitz <marc@mfts.io>
2025-12-01 10:57:40 +0100

    fix: add missing variable for conversation

--- pages/api/links/index.ts
+++ pages/api/links/index.ts
@@ -206,6 +206,7 @@ export default async function handler(
               isFileRequestOnly: linkData.isFileRequestOnly,
               uploadFolderId: linkData.uploadFolderId,
             }),
+            enableConversation: linkData.enableConversation || false,
             showBanner: linkData.showBanner,
             ...(linkData.customFields && {
               customFields: {
